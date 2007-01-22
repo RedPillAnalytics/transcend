8449aefdda5893122400b2691c905f1c9b4db0eb
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-01-22 15:23:19 +0000

    Table for logging standard information messages, expected errors and exceptions.

---
+++ ddl/LOG_TABLE_tbl.sql
@@ -0,0 +1,24 @@
+DROP TABLE common.log_table
+/
+
+CREATE TABLE common.log_table
+       ( entry_ts TIMESTAMP (6) NOT NULL,
+	 session_id NUMBER NOT NULL,
+	 current_scn NUMBER NOT NULL,
+	 instance_name VARCHAR2(30) NOT NULL,
+	 msg VARCHAR2(2000) NOT NULL,
+	 code NUMBER NOT NULL,
+	 client_info VARCHAR2(64) NOT NULL,
+	 module VARCHAR2(48) NOT NULL,
+	 action VARCHAR2(32) NOT NULL,
+	 call_stack VARCHAR2(1024),
+	 back_trace VARCHAR2(1024)
+       ) TABLESPACE common_data
+/
+ALTER TABLE common.log_table ADD CONSTRAINT log_msg_pk PRIMARY KEY (instance_name,session_id, entry_ts)
+      USING INDEX 
+      TABLESPACE common_index  ENABLE
+/
+
+GRANT SELECT ON common.log_table TO job
+/
