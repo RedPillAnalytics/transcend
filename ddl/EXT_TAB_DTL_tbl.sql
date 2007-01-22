dff3df537281a705db9e56a76113a78ba4e2720b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-01-22 20:23:48 +0000

    Replaced COMMON schema with EFW.

--- ddl/EXT_TAB_DTL_tbl.sql
+++ ddl/EXT_TAB_DTL_tbl.sql
@@ -1,7 +1,7 @@
-DROP TABLE common.ext_tab_dtl purge
+DROP TABLE efw.ext_tab_dtl purge
 /
 
-CREATE TABLE common.ext_tab_dtl
+CREATE TABLE efw.ext_tab_dtl
        ( ext_table VARCHAR2(100) NOT NULL,
 	 ext_tab_owner VARCHAR2(100) NOT NULL,
 	 jobname VARCHAR2(50) NOT NULL,
@@ -11,21 +11,21 @@ CREATE TABLE common.ext_tab_dtl
 	 num_lines NUMBER,
 	 reject_pcnt NUMBER,
 	 session_id NUMBER NOT NULL)
-       TABLESPACE common_data
+       TABLESPACE efw
 /
 
-COMMENT ON TABLE common.ext_tab_dtl IS 'Detail information about external tables after File Mover runs.'
+COMMENT ON TABLE efw.ext_tab_dtl IS 'Detail information about external tables after File Mover runs.'
 /
 
-COMMENT ON COLUMN common.ext_tab_dtl.ext_table IS 'Name of the external table.';
-COMMENT ON COLUMN common.ext_tab_dtl.ext_tab_owner IS 'Owner of the external table.';
-COMMENT ON COLUMN common.ext_tab_dtl.jobname IS 'Name of the job that defined in FILE_CTL.';
-COMMENT ON COLUMN common.ext_tab_dtl.jobnumber IS 'Number of the job defined in FILE_CTL.';
-COMMENT ON COLUMN common.ext_tab_dtl.processed_ts IS 'Time the file was processed by File Mover.';
-COMMENT ON COLUMN common.ext_tab_dtl.num_rows IS 'Result of a "SELECT COUNT(*)..." on the table.';
-COMMENT ON COLUMN common.ext_tab_dtl.num_lines IS 'The number of lines at the OS level in the file or files that make up the external table.';
-COMMENT ON COLUMN common.ext_tab_dtl.reject_pcnt IS 'Percentage of records rejected';
-COMMENT ON COLUMN common.ext_tab_dtl.session_id IS 'SESSION_ID of the processing session.';
+COMMENT ON COLUMN efw.ext_tab_dtl.ext_table IS 'Name of the external table.';
+COMMENT ON COLUMN efw.ext_tab_dtl.ext_tab_owner IS 'Owner of the external table.';
+COMMENT ON COLUMN efw.ext_tab_dtl.jobname IS 'Name of the job that defined in FILE_CTL.';
+COMMENT ON COLUMN efw.ext_tab_dtl.jobnumber IS 'Number of the job defined in FILE_CTL.';
+COMMENT ON COLUMN efw.ext_tab_dtl.processed_ts IS 'Time the file was processed by File Mover.';
+COMMENT ON COLUMN efw.ext_tab_dtl.num_rows IS 'Result of a "SELECT COUNT(*)..." on the table.';
+COMMENT ON COLUMN efw.ext_tab_dtl.num_lines IS 'The number of lines at the OS level in the file or files that make up the external table.';
+COMMENT ON COLUMN efw.ext_tab_dtl.reject_pcnt IS 'Percentage of records rejected';
+COMMENT ON COLUMN efw.ext_tab_dtl.session_id IS 'SESSION_ID of the processing session.';
 
-GRANT SELECT ON common.ext_tab_dtl TO file_mover
+GRANT SELECT ON efw.ext_tab_dtl TO efw_filemover
 /
