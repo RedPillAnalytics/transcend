6e3173fef04c876c5ea071748ba2b26a6c1f3335
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-01-22 15:24:22 +0000

    Table for storing SQL statement counts associated with modules and actions.

---
+++ ddl/COUNT_TABLE_tbl.sql
@@ -0,0 +1,23 @@
+DROP TABLE common.count_table purge
+/
+
+CREATE TABLE common.count_table
+(
+  entry_ts       TIMESTAMP,
+  session_id     NUMBER,
+  instance_name  VARCHAR2(30),
+  client_info    VARCHAR2(64),
+  module         VARCHAR2(48),
+  action         VARCHAR2(32),
+  row_cnt        NUMBER
+)
+TABLESPACE common_data
+/
+
+ALTER TABLE common.count_table ADD CONSTRAINT count_tbl_pk PRIMARY KEY (session_id, instance_name, entry_ts)
+    USING INDEX
+    TABLESPACE common_index
+/
+
+GRANT SELECT ON common.count_table TO job
+/
