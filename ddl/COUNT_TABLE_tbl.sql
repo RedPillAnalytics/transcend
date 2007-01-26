a3d9a4627f49193992ab08566e239c645bdfcac5
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-01-26 23:10:40 +0000

    Modified to work with the new APPLOG type.

--- ddl/COUNT_TABLE_tbl.sql
+++ ddl/COUNT_TABLE_tbl.sql
@@ -4,17 +4,16 @@ DROP TABLE efw.count_table purge
 CREATE TABLE efw.count_table
 (
   entry_ts       TIMESTAMP,
-  session_id     NUMBER,
-  instance_name  VARCHAR2(30),
   client_info    VARCHAR2(64),
   module         VARCHAR2(48),
   action         VARCHAR2(32),
+  session_id     NUMBER,
   row_cnt        NUMBER
 )
 TABLESPACE efw
 /
 
-ALTER TABLE efw.count_table ADD CONSTRAINT count_tbl_pk PRIMARY KEY (session_id, instance_name, entry_ts)
+ALTER TABLE efw.count_table ADD CONSTRAINT count_tbl_pk PRIMARY KEY (session_id, entry_ts)
     USING INDEX
     TABLESPACE efw
 /
