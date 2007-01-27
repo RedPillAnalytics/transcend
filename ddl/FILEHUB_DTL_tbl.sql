7aee4c96902464c3cf92525a4fee4f6eb3bc9d28
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-01-27 02:26:32 +0000

    branding file movement piece as FileHub

--- ddl/FILEHUB_DTL_tbl.sql
+++ ddl/FILEHUB_DTL_tbl.sql
@@ -1,17 +1,17 @@
-DROP TABLE efw.file_process_dtl CASCADE CONSTRAINTS purge
+DROP TABLE efw.filehub_dtl CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE efw.file_process_dtl_seq
+DROP SEQUENCE efw.filehub_dtl_seq
 /
 
-CREATE TABLE efw.file_process_dtl
-       ( file_dtl_id NUMBER NOT NULL,
+CREATE TABLE efw.filehub_dtl
+       ( filehub_dtl_id NUMBER NOT NULL,
 	 src_filename VARCHAR2(200),
 	 trg_filename VARCHAR2(200),
-	 file_process_type VARCHAR2(7) NOT null,
+	 filehub_type VARCHAR2(7) NOT null,
 	 arch_filename VARCHAR2(100) NOT NULL,
 	 jobname VARCHAR2(50) NOT NULL,
-	 file_process_id NUMBER NOT NULL,
+	 filehub_id NUMBER NOT NULL,
 	 num_bytes NUMBER NOT null,
 	 num_lines NUMBER,
 	 file_dt DATE NOT null,
@@ -22,26 +22,26 @@ CREATE TABLE efw.file_process_dtl
        TABLESPACE efw
 /
 
-COMMENT ON TABLE efw.file_process_dtl IS 'detail information about each file that is processed by the File Package'
+COMMENT ON TABLE efw.filehub_dtl IS 'detail information about each file that is processed by the File Package'
 /
 
-COMMENT ON COLUMN efw.file_process_dtl.file_dtl_id IS 'sequence generated number as primary key';
-COMMENT ON COLUMN efw.file_process_dtl.src_filename IS 'absolute path of the source file (applicable for feeds only)';
-COMMENT ON COLUMN efw.file_process_dtl.trg_filename IS 'absolute path of the target file... external table location for feeds and extract file name for extracts.';
-COMMENT ON COLUMN efw.file_process_dtl.file_process_type IS 'defines whether the process was a feed or an extract.';
-COMMENT ON COLUMN efw.file_process_dtl.arch_filename IS 'absolute path of the archived version of the file';
-COMMENT ON COLUMN efw.file_process_dtl.jobname IS 'job that called the File Package';
-COMMENT ON COLUMN efw.file_process_dtl.file_process_id IS 'primary key of the FILE_PROCESS_CONF table';
-COMMENT ON COLUMN efw.file_process_dtl.num_bytes IS 'size in bytes of the file';
-COMMENT ON COLUMN efw.file_process_dtl.num_lines IS 'number of lines in the file';
-COMMENT ON COLUMN efw.file_process_dtl.file_dt IS 'last modified date on the file';
-COMMENT ON COLUMN efw.file_process_dtl.processed_ts IS 'date the file was processed by File Package';
-COMMENT ON COLUMN efw.file_process_dtl.ext_tab_ind IS 'Y/N indicator of whether this file will used as an external table';
-COMMENT ON COLUMN efw.file_process_dtl.alt_ext_tab_ind IS 'Y/N indicator of whether the external table containing this file should be altered to include';
-COMMENT ON COLUMN efw.file_process_dtl.session_id IS 'AUD_SID number of the oracle session';
+COMMENT ON COLUMN efw.filehub_dtl.filehub_dtl_id IS 'sequence generated number as primary key';
+COMMENT ON COLUMN efw.filehub_dtl.src_filename IS 'absolute path of the source file (applicable for feeds only)';
+COMMENT ON COLUMN efw.filehub_dtl.trg_filename IS 'absolute path of the target file... external table location for feeds and extract file name for extracts.';
+COMMENT ON COLUMN efw.filehub_dtl.filehub_type IS 'defines whether the process was a feed or an extract.';
+COMMENT ON COLUMN efw.filehub_dtl.arch_filename IS 'absolute path of the archived version of the file';
+COMMENT ON COLUMN efw.filehub_dtl.jobname IS 'job that called the File Package';
+COMMENT ON COLUMN efw.filehub_dtl.filehub_id IS 'primary key of the FILEHUB_CONF table';
+COMMENT ON COLUMN efw.filehub_dtl.num_bytes IS 'size in bytes of the file';
+COMMENT ON COLUMN efw.filehub_dtl.num_lines IS 'number of lines in the file';
+COMMENT ON COLUMN efw.filehub_dtl.file_dt IS 'last modified date on the file';
+COMMENT ON COLUMN efw.filehub_dtl.processed_ts IS 'date the file was processed by File Package';
+COMMENT ON COLUMN efw.filehub_dtl.ext_tab_ind IS 'Y/N indicator of whether this file will used as an external table';
+COMMENT ON COLUMN efw.filehub_dtl.alt_ext_tab_ind IS 'Y/N indicator of whether the external table containing this file should be altered to include';
+COMMENT ON COLUMN efw.filehub_dtl.session_id IS 'AUDSID number of the oracle session';
 
-GRANT SELECT ON EFW.FILE_PROCESS_DTL TO efw_file;
+GRANT SELECT ON EFW.FILEHUB_DTL TO efw_file;
 /
 
-CREATE SEQUENCE efw.file_process_dtl_seq
+CREATE SEQUENCE efw.filehub_dtl_seq
 /
--- ddl/FILEHUB_EXTRACT_CONF_tbl.sql
+++ ddl/FILEHUB_EXTRACT_CONF_tbl.sql
@@ -1,8 +1,8 @@
-DROP TABLE efw.file_extract_conf CASCADE CONSTRAINTS purge
+DROP TABLE efw.fh_extract_conf CASCADE CONSTRAINTS purge
 /
 
-CREATE TABLE efw.file_extract_conf
-       ( file_process_id NUMBER		NOT NULL,
+CREATE TABLE efw.fh_extract_conf
+       ( fh_id NUMBER		NOT NULL,
 	 dateformat      VARCHAR2(30)   NOT NULL,
 	 delimiter       VARCHAR2(5)    NOT NULL,
 	 quotechar       VARCHAR2(1) 	NOT NULL,
@@ -19,27 +19,27 @@ CREATE TABLE efw.file_extract_conf
 TABLESPACE efw
 /
 
-COMMENT ON TABLE efw.file_extract_conf IS 'table holding configuration information for EXTRACTS in the FILE package';
+COMMENT ON TABLE efw.fh_extract_conf IS 'table holding configuration information for EXTRACTS in the FILE package';
 
-COMMENT ON COLUMN efw.file_extract_conf.file_process_id IS 'sequence generated number that distinctly identifies a file process. Primary key of the table.';
-COMMENT ON COLUMN efw.file_extract_conf.dateformat IS 'NLS_DATE_FORMAT of date columns in the extract';
-COMMENT ON COLUMN efw.file_extract_conf.delimiter IS 'delimiter used to separate columns';
-COMMENT ON COLUMN efw.file_extract_conf.quotechar IS 'quotechar used to support columns. An NA specifies that no quotechar is used';
-COMMENT ON COLUMN efw.file_extract_conf.headers IS 'a Y/N indicator of whether headers should be included as the first row in the filw';
-COMMENT ON COLUMN efw.file_extract_conf.sendmail IS 'a Y/N indicator of whether to send emails announcing the extracts';
-COMMENT ON COLUMN efw.file_extract_conf.sender IS 'sender of file announcement emails';
-COMMENT ON COLUMN efw.file_extract_conf.recipients IS 'comma separated list of email addresses that receive file announcement emails';
-COMMENT ON COLUMN efw.file_extract_conf.baseurl IS 'baseurl that serves up the extract file';
-COMMENT ON COLUMN efw.file_extract_conf.created_user IS 'for auditing';
-COMMENT ON COLUMN efw.file_extract_conf.created_dt IS 'for auditing';
-COMMENT ON COLUMN efw.file_extract_conf.modified_user IS 'for auditing';
-COMMENT ON COLUMN efw.file_extract_conf.modified_dt IS 'for auditing';
+COMMENT ON COLUMN efw.fh_extract_conf.fh_id IS 'sequence generated number that distinctly identifies a file process. Primary key of the table.';
+COMMENT ON COLUMN efw.fh_extract_conf.dateformat IS 'NLS_DATE_FORMAT of date columns in the extract';
+COMMENT ON COLUMN efw.fh_extract_conf.delimiter IS 'delimiter used to separate columns';
+COMMENT ON COLUMN efw.fh_extract_conf.quotechar IS 'quotechar used to support columns. An NA specifies that no quotechar is used';
+COMMENT ON COLUMN efw.fh_extract_conf.headers IS 'a Y/N indicator of whether headers should be included as the first row in the filw';
+COMMENT ON COLUMN efw.fh_extract_conf.sendmail IS 'a Y/N indicator of whether to send emails announcing the extracts';
+COMMENT ON COLUMN efw.fh_extract_conf.sender IS 'sender of file announcement emails';
+COMMENT ON COLUMN efw.fh_extract_conf.recipients IS 'comma separated list of email addresses that receive file announcement emails';
+COMMENT ON COLUMN efw.fh_extract_conf.baseurl IS 'baseurl that serves up the extract file';
+COMMENT ON COLUMN efw.fh_extract_conf.created_user IS 'for auditing';
+COMMENT ON COLUMN efw.fh_extract_conf.created_dt IS 'for auditing';
+COMMENT ON COLUMN efw.fh_extract_conf.modified_user IS 'for auditing';
+COMMENT ON COLUMN efw.fh_extract_conf.modified_dt IS 'for auditing';
 
 
-ALTER TABLE efw.file_extract_conf ADD (
-  CONSTRAINT file_extract_conf_pk
+ALTER TABLE efw.fh_extract_conf ADD (
+  CONSTRAINT fh_extract_conf_pk
  PRIMARY KEY
- (file_process_id)
+ (fh_id)
     USING INDEX
     TABLESPACE efw)
 /
