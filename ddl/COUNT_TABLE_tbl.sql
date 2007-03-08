ffbd6303283f34cf8caf13b28ed8739713e2b70e
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-08 07:33:26 +0000

    Modified default columns for ENTRY_TS.

--- ddl/COUNT_TABLE_tbl.sql
+++ ddl/COUNT_TABLE_tbl.sql
@@ -3,12 +3,12 @@ DROP TABLE tdinc.count_table purge
 
 CREATE TABLE tdinc.count_table
 (
-  entry_ts       TIMESTAMP,
-  client_info    VARCHAR2(64),
-  module         VARCHAR2(48),
-  action         VARCHAR2(32),
-  session_id     NUMBER,
-  row_cnt        NUMBER
+  entry_ts       TIMESTAMP DEFAULT systimestamp NOT null,
+  client_info    VARCHAR2(64) NOT null,
+  module         VARCHAR2(48) NOT null,
+  action         VARCHAR2(32) NOT null,
+  session_id     NUMBER NOT null,
+  row_cnt        NUMBER NOT null
 )
 TABLESPACE tdinc
 /
