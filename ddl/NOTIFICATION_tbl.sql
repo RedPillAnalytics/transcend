11155d7db30c9a4a5613eb7cd9bc4d5bbd8e1abf
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-15 05:16:17 +0000

    Modified the name and the columns to be specific to sending email.
    Other forms of notification will have their own table if developed.

--- ddl/NOTIFICATION_tbl.sql
+++ ddl/NOTIFICATION_tbl.sql
@@ -1,17 +1,15 @@
-DROP TABLE tdinc.notification CASCADE CONSTRAINTS purge
+DROP TABLE tdinc.email_notify CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE tdinc.notification_seq
+DROP SEQUENCE tdinc.email_notify_seq
 /
 
-CREATE TABLE tdinc.notification
-       ( notification_id NUMBER NOT NULL,
-	 notification_type VARCHAR2(30) NOT NULL,
+CREATE TABLE tdinc.email_notify
+       ( email_notify_id NUMBER NOT NULL,
 	 sender         VARCHAR2(1024) NOT NULL,
 	 recipients     VARCHAR2(2000) NOT NULL,
 	 subject VARCHAR2(100) NOT NULL,
 	 message VARCHAR2(2000) NOT NULL,
-	 baseurl        VARCHAR2(255) NOT NULL,
 	 created_user   VARCHAR2(30) NOT NULL,
 	 created_dt     DATE NOT NULL,
 	 modified_user  VARCHAR2(30),
@@ -20,13 +18,13 @@ CREATE TABLE tdinc.notification
 TABLESPACE tdinc
 /
 
-ALTER TABLE tdinc.notification ADD (
+ALTER TABLE tdinc.email_notify ADD (
   CONSTRAINT notification_pk
  PRIMARY KEY
- (notification_id)
+ (email_notify_id)
     USING INDEX
     TABLESPACE tdinc)
 /
 
-CREATE SEQUENCE tdinc.notification_seq
+CREATE SEQUENCE tdinc.email_notify_seq
 /
