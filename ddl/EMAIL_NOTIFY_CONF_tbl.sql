725c242d6def8554fde0c5b1ddb098a93255b49b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-16 22:58:45 +0000

    Create NOTIFY_CONF and EMAIL_NOTIFY_CONF tables to make the generic NOTIFY procedure the entryway into the NOTIFICATION package.

--- ddl/EMAIL_NOTIFY_CONF_tbl.sql
+++ ddl/EMAIL_NOTIFY_CONF_tbl.sql
@@ -1,11 +1,11 @@
-DROP TABLE tdinc.email_notify CASCADE CONSTRAINTS purge
+DROP TABLE tdinc.email_notify_conf CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE tdinc.email_notify_seq
+DROP SEQUENCE tdinc.email_notify_conf_seq
 /
 
-CREATE TABLE tdinc.email_notify
-       ( email_notify_id NUMBER NOT NULL,
+CREATE TABLE tdinc.email_notify_conf
+       ( email_notifyid NUMBER NOT NULL,
 	 sender         VARCHAR2(1024) NOT NULL,
 	 recipients     VARCHAR2(2000) NOT NULL,
 	 subject VARCHAR2(100) NOT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE tdinc.email_notify
 TABLESPACE tdinc
 /
 
-ALTER TABLE tdinc.email_notify ADD (
-  CONSTRAINT notification_pk
+ALTER TABLE tdinc.email_notify_conf ADD (
+  CONSTRAINT email_notify_conf_pk
  PRIMARY KEY
- (email_notify_id)
+ (email_notify_cd)
     USING INDEX
     TABLESPACE tdinc)
 /
 
-CREATE SEQUENCE tdinc.email_notify_seq
+CREATE SEQUENCE tdinc.email_notify_conf_seq
 /
