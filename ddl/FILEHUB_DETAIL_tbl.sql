1b69ed5b3f39341de5e0d573f3fb7bf9e52eb5c1
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-24 23:18:29 +0000

    New column names SOURCE_FILEPATH, TARGET_FILEPATH and ARCH_FILEPATH.

--- ddl/FILEHUB_DETAIL_tbl.sql
+++ ddl/FILEHUB_DETAIL_tbl.sql
@@ -10,9 +10,9 @@ CREATE TABLE tdinc.filehub_detail
 	 filehub_name VARCHAR2(50),
 	 filehub_group VARCHAR2(64),
 	 filehub_type VARCHAR2(7) NOT null,
-	 src_filename VARCHAR2(200),
-	 trg_filename VARCHAR2(200),
-	 arch_filename VARCHAR2(100) NOT NULL,
+	 source_filepath VARCHAR2(200),
+	 target_filepath VARCHAR2(200),
+	 arch_filepath VARCHAR2(100) NOT NULL,
 	 num_bytes NUMBER NOT null,
 	 num_lines NUMBER,
 	 file_dt DATE NOT null,
@@ -29,9 +29,9 @@ COMMENT ON COLUMN tdinc.filehub_detail.filehub_id IS 'parent key from the FILEHU
 COMMENT ON COLUMN tdinc.filehub_detail.filehub_name IS 'from the FILEHUB_CONF table';
 COMMENT ON COLUMN tdinc.filehub_detail.filehub_group IS 'from the FILEHUB_CONF table';
 COMMENT ON COLUMN tdinc.filehub_detail.filehub_type IS 'from the FILEHUB_CONF table';
-COMMENT ON COLUMN tdinc.filehub_detail.src_filename IS 'absolute path of the source file (applicable for feeds only)';
-COMMENT ON COLUMN tdinc.filehub_detail.trg_filename IS 'absolute path of the target file... external table location for feeds and extract file name for extracts.';
-COMMENT ON COLUMN tdinc.filehub_detail.arch_filename IS 'absolute path of the archived version of the file';
+COMMENT ON COLUMN tdinc.filehub_detail.source_filepath IS 'absolute path of the source file (applicable for feeds only)';
+COMMENT ON COLUMN tdinc.filehub_detail.target_filepath IS 'absolute path of the target file... external table location for feeds and extract file name for extracts.';
+COMMENT ON COLUMN tdinc.filehub_detail.arch_filepath IS 'absolute path of the archived version of the file';
 COMMENT ON COLUMN tdinc.filehub_detail.num_bytes IS 'size in bytes of the file';
 COMMENT ON COLUMN tdinc.filehub_detail.num_lines IS 'number of lines in the file';
 COMMENT ON COLUMN tdinc.filehub_detail.file_dt IS 'last modified date on the file';
