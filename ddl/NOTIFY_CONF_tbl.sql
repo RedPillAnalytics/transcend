c189f9cdf8554ad7fc95e2fbb6ab7742ac3e317f
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-29 16:57:22 +0000

    Major rewrite... I won't go into all changes.
    BASETYPE was added as the underlying super type that all things inherit from. It contains the DEBUG functionality previously in NOTIFY.
    
    Notify is no longer the super type... it is instead a final type that is instantiated in the other types.
    
    The NOTIFY type was rewriten to handle differnet components, not operating on the MODULE and ACTION of a calling method.
    
    The underlying configuration tables have been modified to support these changes.

--- ddl/NOTIFY_CONF_tbl.sql
+++ ddl/NOTIFY_CONF_tbl.sql
@@ -5,11 +5,13 @@ DROP SEQUENCE tdinc.notify_conf_seq
 /
 
 CREATE TABLE tdinc.notify_conf
-       ( notify_id NUMBER NOT NULL,
-	 notify_type VARCHAR2(10),
-	 notify_type_id NUMBER,
-	 message                VARCHAR2(2000),
-	 subject                VARCHAR2(100),
+       ( notify_id	NUMBER NOT NULL,
+	 notify_method 	VARCHAR2(20),
+	 action 	VARCHAR2(32),
+	 module 	VARCHAR2(48),
+	 module_id 	NUMBER,
+	 message   	VARCHAR2(2000),
+	 subject   	VARCHAR2(100),
 	 created_user   VARCHAR2(30) NOT NULL,
 	 created_dt     DATE NOT NULL,
 	 modified_user  VARCHAR2(30),
@@ -22,8 +24,7 @@ COMMENT ON TABLE tdinc.notify_conf IS 'table to hold generic notification inform
 
 COMMENT ON COLUMN tdinc.notify_conf.message IS 'The default message body text. This is text that shows up in the email, but the module that sends the notification might add additional information to it';
 COMMENT ON COLUMN tdinc.notify_conf.subject IS 'The default subject text. This is text that shows up in the email, but the module that sends the notification might add additional information to it';
-COMMENT ON COLUMN tdinc.notify_conf.notify_type IS 'the type of notification; currently, only "email" is supported. This column also points to the supporting conf table.';
-COMMENT ON COLUMN tdinc.notify_conf.notify_type_id IS 'the id of the supporting configuration table.';
+COMMENT ON COLUMN tdinc.notify_conf.notify_method IS 'the type of notification; currently, only "email" is supported. This column also points to the supporting conf table.';
 
 ALTER TABLE tdinc.notify_conf ADD (
   CONSTRAINT notify_conf_pk
