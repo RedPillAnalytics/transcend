a81cb82b46466a042fb08736a520cf656f9b615e
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-29 16:58:21 +0000

    New directory to hold object-relation views.

---
+++ object_views/EXTRACT_OT_vw.sql
@@ -0,0 +1,84 @@
+CREATE OR REPLACE VIEW tdinc.extract_ot 
+OF tdinc.extract
+WITH object identifier (filehub_id)
+as
+SELECT cast('N' AS VARCHAR2(1)) debug,
+       filehub_id,
+       filehub_name,
+       filehub_group,
+       filehub_type,
+       object_owner,
+       object_name,
+       directory,
+       dirpath,
+       filename,
+       coreutils.get_dir_path (directory) || '/' || filename filepath,
+       arch_directory,
+       arch_dirpath,
+       arch_filename,
+       coreutils.get_dir_path (arch_directory) || '/' || arch_filename arch_filepath,
+       file_datestamp,
+       min_bytes,
+       max_bytes,
+       baseurl,
+       CASE baseurl
+       WHEN null
+       THEN null
+       ELSE 
+       baseurl||'/'||filename 
+       END file_url,
+       passphrase,
+       'alter session set nls_date_format=''' || dateformat || '''' dateformat_ddl,
+       'alter session set nls_timestamp_format=''' || timestampformat || '''' tsformat_ddl,
+       delimiter,
+       quotechar,
+       headers
+  FROM (SELECT filehub_id,
+               filehub_name filehub_name,
+               filehub_group filehub_group,
+               filehub_type,
+               object_owner,
+               object_name,
+               directory,
+	       coreutils.get_dir_path (directory) dirpath,
+               CASE file_datestamp
+               WHEN 'NA'
+               THEN filename
+               ELSE regexp_replace (filename,
+                                     '\.',
+                                     '_'
+                                     || to_char (SYSDATE,
+                                                  file_datestamp)
+                                     || '.')
+               END filename,
+               CASE file_datestamp
+               WHEN 'NA'
+               THEN  filename
+               || '.'
+               || to_char (SYSDATE, 'yyyymmddhhmiss')
+               ELSE regexp_replace (filename,
+                                     '\.',
+                                     '_'
+                                     || to_char (SYSDATE,
+                                                  file_datestamp)
+                                     || '.')
+               END arch_filename,
+               arch_directory,
+	       coreutils.get_dir_path (arch_directory) arch_dirpath,
+	       file_datestamp,
+               min_bytes,
+               max_bytes,
+               baseurl,
+	       passphrase,
+               dateformat,
+               timestampformat,
+               delimiter,
+               CASE
+               WHEN quotechar = 'NA'
+               THEN NULL
+               WHEN quotechar IS NOT NULL
+               THEN quotechar
+               END quotechar,
+               headers
+          FROM tdinc.filehub_conf
+	 WHERE REGEXP_LIKE (filehub_type, '^extract$', 'i'));
