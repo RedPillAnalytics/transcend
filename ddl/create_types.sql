785312b919b9616ab990e52b489589b3113a6507
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-29 16:57:32 +0000

    Major rewrite... I won't go into all changes.
    BASETYPE was added as the underlying super type that all things inherit from. It contains the DEBUG functionality previously in NOTIFY.
    
    Notify is no longer the super type... it is instead a final type that is instantiated in the other types.
    
    The NOTIFY type was rewriten to handle differnet components, not operating on the MODULE and ACTION of a calling method.
    
    The underlying configuration tables have been modified to support these changes.

--- ddl/create_types.sql
+++ ddl/create_types.sql
@@ -1,6 +1,12 @@
 DROP TYPE tdinc.extract;
+DROP TYPE tdinc.feed;
+DROP TYPE tdinc.fhconf;
 
-@../types/FILECONF.tps
-@../types/FILECONF.tpb
-@../types/EXTRACT.tps
-@../types/EXTRACT.tpb
+@C:/cygwin/home/brysons/source/framework/trunc/types/NOTIFY.tps
+@C:/cygwin/home/brysons/source/framework/trunc/types/NOTIFY.tpb
+@C:/cygwin/home/brysons/source/framework/trunc/types/FILECONF.tps
+@C:/cygwin/home/brysons/source/framework/trunc/types/FILECONF.tpb
+@C:/cygwin/home/brysons/source/framework/trunc/types/EXTRACT.tps
+@C:/cygwin/home/brysons/source/framework/trunc/types/EXTRACT.tpb
+@C:/cygwin/home/brysons/source/framework/trunc/types/FEED.tps
+@C:/cygwin/home/brysons/source/framework/trunc/types/FEED.tpb
