8b48294d12bd4d43c6cf39139b595df408f8360a
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-03-29 16:57:13 +0000

    Major rewrite... I won't go into all changes.
    BASETYPE was added as the underlying super type that all things inherit from. It contains the DEBUG functionality previously in NOTIFY.
    
    Notify is no longer the super type... it is instead a final type that is instantiated in the other types.
    
    The NOTIFY type was rewriten to handle differnet components, not operating on the MODULE and ACTION of a calling method.
    
    The underlying configuration tables have been modified to support these changes.

--- ddl/ERR_CD_tbl.sql
+++ ddl/ERR_CD_tbl.sql
@@ -31,5 +31,6 @@ INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20010, 'file_too_big','Fil
 INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20011, 'file_too_small','File size smaller than MAX_BYTES paramter');
 INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20012, 'notify_method_invalid','The notification method is not valid');
 INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20013, 'no_files_found','No files found for this configuration');
-INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20014, 'no_ext_tab','External table in this configuration is non-existent');
-INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20014, 'no_ext_files','There are no files found for this external table.');
+INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20015, 'no_ext_tab','External table in this configuration is non-existent');
+INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20016, 'no_ext_files','There are no files found for this external table.');
+INSERT INTO tdinc.err_cd (code,name,message) VALUES (-20017, 'reject_limit_exceeded','The external table reject limit was exceeded.');
