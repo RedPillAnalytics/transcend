565d2ccf6cd18681cacd01a2e42b9fd13931ea47
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-04-15 03:38:12 +0000

    The SENDER and RECIPIENTS column were added here. I decided to no longer have the EMAIL_NOTIFY_CONF table. Instead, all needed columns about notification will go here.
    If a new notification method is added, then additional columns will be added to the NOTIFY_CONF table, and a new type will be created that extends notify.
    
    This is what was done with the EMAIL type. There is an object relational view called EMAIL_OT which hides all the columns not used by the EMAIL type.

--- ddl/NOTIFY_CONF_tbl.sql
+++ ddl/NOTIFY_CONF_tbl.sql
@@ -5,18 +5,21 @@ DROP SEQUENCE tdinc.notify_conf_seq
 /
 
 CREATE TABLE tdinc.notify_conf
-       ( notify_id	NUMBER NOT NULL,
-	 notify_method 	VARCHAR2(20),
-	 action 	VARCHAR2(32),
-	 module 	VARCHAR2(48),
-	 module_id 	NUMBER,
-	 message   	VARCHAR2(2000),
-	 subject   	VARCHAR2(100),
-	 created_user   VARCHAR2(30) NOT NULL,
-	 created_dt     DATE NOT NULL,
-	 modified_user  VARCHAR2(30),
-	 modified_dt    DATE
-)
+       ( notify_id	  NUMBER NOT NULL,
+	 notify_method 	  VARCHAR2(20) NOT NULL,
+	 notify_enabled	  VARCHAR2(3) NOT NULL,
+	 message          VARCHAR2(2000),
+	 subject   	  VARCHAR2(100),
+	 sender           VARCHAR2(1024) NOT NULL,
+	 recipients       VARCHAR2(2000) NOT NULL,
+	 action 	  VARCHAR2(32),
+	 module 	  VARCHAR2(48),
+	 module_id 	  NUMBER,
+	 created_user     VARCHAR2(30) NOT NULL,
+	 created_dt       DATE NOT NULL,
+	 modified_user    VARCHAR2(30),
+	 modified_dt	  DATE
+       )
 /
 
 COMMENT ON TABLE tdinc.notify_conf IS 'table to hold generic notification information regardless of the notification method';
@@ -34,3 +37,96 @@ ALTER TABLE tdinc.notify_conf ADD (
 
 CREATE SEQUENCE tdinc.notify_conf_seq
 /
+
+INSERT INTO tdinc.notify_conf
+       ( notify_id, 
+	 notify_method, 
+	 notify_enabled, 
+	 message, 
+	 subject, 
+	 sender, 
+	 recipients, 
+	 action, 
+	 module, 
+	 module_id, 
+	 created_user, 
+	 created_dt, 
+	 modified_user, 
+	 modified_dt
+       ) 
+       VALUES ( tdinc.notify_conf_seq.nextval,
+		'email',
+		'yes',
+		'test feed file received',
+		'test feed file received',
+		'stewart.bryson@transcendentdat.com',
+		'stewartbryson@gmail.com,stewart.bryson@transcendentdata.com',
+		'notify success',
+		'feed.process',
+		null,
+		sys_context('USERENV','SESSION_USER'),
+		SYSDATE,
+		NULL,
+		NULL);
+
+INSERT INTO tdinc.notify_conf
+       ( notify_id, 
+	 notify_method, 
+	 notify_enabled, 
+	 message, 
+	 subject, 
+	 sender, 
+	 recipients, 
+	 action, 
+	 module, 
+	 module_id, 
+	 created_user, 
+	 created_dt, 
+	 modified_user, 
+	 modified_dt
+       ) 
+       VALUES ( tdinc.notify_conf_seq.nextval,
+		'email',
+		'yes',
+		'Source file reject limit exceeded',
+		'Source file reject limit exceeded',
+		'stewart.bryson@transcendentdat.com',
+		'stewartbryson@gmail.com,stewart.bryson@transcendentdata.com',
+		'reject limit exceeded',
+		'feed.process',
+		null,
+		sys_context('USERENV','SESSION_USER'),
+		SYSDATE,
+		NULL,
+		NULL);
+
+INSERT INTO tdinc.notify_conf
+       ( notify_id, 
+	 notify_method, 
+	 notify_enabled, 
+	 message, 
+	 subject, 
+	 sender, 
+	 recipients, 
+	 action, 
+	 module, 
+	 module_id, 
+	 created_user, 
+	 created_dt, 
+	 modified_user, 
+	 modified_dt
+       ) 
+       VALUES ( tdinc.notify_conf_seq.nextval,
+		'email',
+		'yes',
+		'test extract ready for download',
+		'test extract ready for download',
+		'stewart.bryson@transcendentdat.com',
+		'stewartbryson@gmail.com,stewart.bryson@transcendentdata.com',
+		'notify success',
+		'extract.process',
+		null,
+		sys_context('USERENV','SESSION_USER'),
+		SYSDATE,
+		NULL,
+		NULL);
