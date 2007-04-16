a909aaec530907afd6389ef2197239efb6112437
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-04-16 17:51:23 +0000

    Tables for holding configuration information.
    Intially, a default value for MODULE or ACTION was configured with a NULL value. Now, the string "default" is used.

---
+++ ddl/REGISTRATION_CONF_tbl.sql
@@ -0,0 +1,46 @@
+DROP TABLE tdinc.registration_conf CASCADE CONSTRAINTS purge
+/
+
+DROP SEQUENCE tdinc.registration_conf_seq
+/
+
+CREATE TABLE tdinc.registration_conf
+       ( registration_id     NUMBER NOT NULL,
+	 registration  	     VARCHAR2(10) not NULL,
+	 action 	     VARCHAR2(32),
+	 module 	     VARCHAR2(48),
+	 created_user	     VARCHAR2(30) NOT NULL,
+	 created_dt	     DATE NOT NULL,
+	 modified_user	     VARCHAR2(30),
+	 modified_dt	     DATE
+       )
+/
+
+ALTER TABLE tdinc.registration_conf ADD (
+  CONSTRAINT registration_conf_pk
+ PRIMARY KEY
+ (registration_id)
+    USING INDEX)
+/
+
+CREATE SEQUENCE tdinc.registration_conf_seq
+/
+
+INSERT INTO tdinc.registration_conf
+       ( registration_id, 
+	 registration, 
+	 action, 
+	 module, 
+	 created_user, 
+	 created_dt, 
+	 modified_user, 
+	 modified_dt
+       )
+       VALUES ( tdinc.registration_conf_seq.nextval,
+		'register',
+		'default',
+		'default',
+		sys_context('USERENV','SESSION_USER'),
+		SYSDATE,
+		NULL,
+		NULL);
