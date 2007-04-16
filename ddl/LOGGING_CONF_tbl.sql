230d1ed37d37a2bd70125079a99a5eaa54c83462
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-04-16 17:51:18 +0000

    Tables for holding configuration information.
    Intially, a default value for MODULE or ACTION was configured with a NULL value. Now, the string "default" is used.

--- ddl/LOGGING_CONF_tbl.sql
+++ ddl/LOGGING_CONF_tbl.sql
@@ -8,20 +8,14 @@ CREATE TABLE tdinc.logging_conf
        ( logging_id	  NUMBER NOT NULL,
 	 logging_level    NUMBER not NULL,
 	 debug_level 	  NUMBER NOT NULL,
-	 action 	  VARCHAR2(32),
-	 module 	  VARCHAR2(48),
-	 created_user     VARCHAR2(30) NOT NULL,
-	 created_dt       DATE NOT NULL,
+	 action 	  VARCHAR2(32) NOT NULL,
+	 module 	  VARCHAR2(48) NOT NULL,
+	 created_user     VARCHAR2(30) DEFAULT sys_context('USERENV','SESSION_USER') NOT NULL,
+	 created_dt       DATE DEFAULT SYSDATE NOT NULL,
 	 modified_user    VARCHAR2(30),
 	 modified_dt	  DATE
        )
 /
-
-COMMENT ON TABLE tdinc.logging_conf IS 'table to hold generic notification information regardless of the notification method';
-
-COMMENT ON COLUMN tdinc.logging_conf.message IS 'The default message body text. This is text that shows up in the email, but the module that sends the notification might add additional information to it';
-COMMENT ON COLUMN tdinc.logging_conf.subject IS 'The default subject text. This is text that shows up in the email, but the module that sends the notification might add additional information to it';
-COMMENT ON COLUMN tdinc.logging_conf.notify_method IS 'the type of notification; currently, only "email" is supported. This column also points to the supporting conf table.';
 
 ALTER TABLE tdinc.logging_conf ADD (
   CONSTRAINT logging_conf_pk
@@ -47,8 +41,8 @@ INSERT INTO tdinc.logging_conf
        VALUES ( tdinc.logging_conf_seq.nextval,
 		2,
 		4,
-		null,
-		null,
+		'default',
+		'default',
 		sys_context('USERENV','SESSION_USER'),
 		SYSDATE,
 		NULL,
