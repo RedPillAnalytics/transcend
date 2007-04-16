ff0cf3fe85e4423322bb813f0debb0838e0ec26f
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-04-16 17:51:27 +0000

    Tables for holding configuration information.
    Intially, a default value for MODULE or ACTION was configured with a NULL value. Now, the string "default" is used.

---
+++ ddl/RUNMODE_CONF_tbl.sql
@@ -0,0 +1,46 @@
+DROP TABLE tdinc.runmode_conf CASCADE CONSTRAINTS purge
+/
+
+DROP SEQUENCE tdinc.runmode_conf_seq
+/
+
+CREATE TABLE tdinc.runmode_conf
+       ( runmode_id	  NUMBER NOT NULL,
+	 default_runmode  VARCHAR2(10) not NULL,
+	 action 	  VARCHAR2(32),
+	 module 	  VARCHAR2(48),
+	 created_user     VARCHAR2(30) NOT NULL,
+	 created_dt       DATE NOT NULL,
+	 modified_user    VARCHAR2(30),
+	 modified_dt	  DATE
+       )
+/
+
+ALTER TABLE tdinc.runmode_conf ADD (
+  CONSTRAINT runmode_conf_pk
+ PRIMARY KEY
+ (runmode_id)
+    USING INDEX)
+/
+
+CREATE SEQUENCE tdinc.runmode_conf_seq
+/
+
+INSERT INTO tdinc.runmode_conf
+       ( runmode_id, 
+	 default_runmode, 
+	 action, 
+	 module, 
+	 created_user, 
+	 created_dt, 
+	 modified_user, 
+	 modified_dt
+       )
+       VALUES ( tdinc.runmode_conf_seq.nextval,
+		'runtime',
+		'default',
+		'default',
+		sys_context('USERENV','SESSION_USER'),
+		SYSDATE,
+		NULL,
+		NULL);
