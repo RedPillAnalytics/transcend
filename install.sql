e29052b2883fd6fd74fc17a4d01b98d1f3774478
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-04-25 12:56:00 +0000

    Most recent additions to the framework are now included in the install script.

--- install.sql
+++ install.sql
@@ -7,7 +7,7 @@ SET echo on
 -- /
 
 -- CREATE TABLESPACE tdinc
--- DATAFILE 'C:/oracle/product/10.2.0/oradata/wtdinctdinc_01.dbf'
+-- DATAFILE '/u01/oradata/tdinc_01.dbf'
 -- SIZE 1M
 -- AUTOEXTEND ON NEXT 1M MAXSIZE 20M
 -- EXTENT MANAGEMENT LOCAL AUTOALLOCATE SEGMENT SPACE MANAGEMENT AUTO;
@@ -34,46 +34,69 @@ SET echo on
 -- CREATE ROLE tdinc_dbflex;
 -- CREATE ROLE tdinc_applog;
 
-
-PROMPT 'First create the tdinc tables'
+--First create the tdinc tables
 @./ddl/COUNT_TABLE_tbl
 @./ddl/LOG_TABLE_tbl
 @./ddl/DIR_LIST_tbl
 @./ddl/FILEHUB_CONF_tbl
 @./ddl/FILEHUB_DETAIL_tbl
 @./ddl/FILEHUB_OBJ_DETAIL_tbl
-@./ddl/NOTIFICATION_tbl
+@./ddl/PARTNAME_tbl.sql
+@./ddl/PARAMETER_CONF_tbl.sql
+@./ddl/NOTIFY_CONF_tbl.sql
+@./ddl/LOGGING_CONF_tbl.sql
+@./ddl/RUNMODE_CONF_tbl.sql
+@./ddl/REGISTRATION_CONF_tbl.sql
+@./ddl/ERR_CD_tbl.sql
 
-PROMPT 'create java stored procedures'
+--create java stored procedure
 @./java/CoreUtils.jvs
 
-PROMPT 'create types'
+--drop all types due to inheritance
+DROP TYPE tdinc.feed;
+DROP TYPE tdinc.extract;
+DROP TYPE tdinc.fhconf;
+DROP TYPE tdinc.email;
+DROP TYPE tdinc.notify;
+DROP TYPE tdinc.applog;
+
+--create core pieces
 @./types/STRING_AGG_TYPE.tps
 @./types/STRING_AGG_TYPE.tpb
-@./types/APPLOG.tps
-@./types/APPLOG.tpb
-
-PROMPT 'create custom aggregate function'
 @./plsql/STRAGG.fnc
-
-PROMPT 'create packages'
 @./plsql/COREUTILS.pks
 @./plsql/COREUTILS.pkb
+
+--create targeted types
+@./types/BASETYPE.tps
+@./types/BASETYPE.tpb
+@./types/APPLOG.tps
+@./types/APPLOG.tpb
+@./types/NOTIFY.tps
+@./types/EMAIL.tps
+@./types/EMAIL.tpb
+@./types/FHCONF.tps
+@./types/FHCONF.tpb
+@./types/EXTRACT.tps
+@./types/EXTRACT.tpb
+@./types/FEED.tps
+@./types/FEED.tpb
+
+--create callable packages
 @./plsql/DBFLEX.pks
 @./plsql/DBFLEX.pkb
 @./plsql/FILEHUB.pks
 @./plsql/FILEHUB.pkb
+@./plsql/CONTROL.pks
+@./plsql/CONTROL.pkb
 
-PROMPT 'public synonyms'
-CREATE OR REPLACE PUBLIC SYNONYM coreutils FOR tdinc.coreutils;
+--public synonyms
 CREATE OR REPLACE PUBLIC SYNONYM filehub FOR tdinc.filehub;
 CREATE OR REPLACE PUBLIC SYNONYM log_table FOR tdinc.log_table;
 CREATE OR REPLACE PUBLIC SYNONYM count_table FOR tdinc.count_table;
 CREATE OR REPLACE PUBLIC SYNONYM stragg FOR tdinc.stragg;
-CREATE OR REPLACE PUBLIC SYNONYM notify for tdinc.notify;
 
-
-PROMPT 'java permissions'
+--java permissions
 EXEC dbms_java.set_output(1000000);
 EXEC dbms_java.grant_permission( 'TDINC', 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'execute' );
 EXEC dbms_java.grant_permission( 'TDINC', 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'read' );
