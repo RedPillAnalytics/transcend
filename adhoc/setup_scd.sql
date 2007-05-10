b12c987d4604a56167ca528cfdcb4266ddfd1063
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-10 20:15:55 +0000

    testing files

---
+++ adhoc/setup_scd.sql
@@ -0,0 +1,48 @@
+DROP TABLE test_dim
+/
+
+CREATE TABLE TEST_DIM
+   (    TEST_KEY NUMBER,
+        NAT_KEY NUMBER,
+        BIRTHDATE DATE,
+        NAME VARCHAR2(50),
+        EFFECTIVE_START_DT DATE,
+        EFFECTIVE_END_DT DATE,
+        CURRENT_IND VARCHAR2(1)
+   )
+/
+
+DROP TABLE test_stg
+/
+
+CREATE TABLE TEST_STG
+   (    NAT_KEY NUMBER,
+        BIRTHDATE DATE,
+        NAME VARCHAR2(50),
+        EFFECTIVE_START_DT DATE
+   )
+/
+
+DROP SEQUENCE test_dim_seq
+/
+CREATE SEQUENCE TEST_DIM_SEQ START WITH 21
+/
+
+DROP SEQUENCE test_natkey_seq
+/
+CREATE SEQUENCE TEST_NATKEY_SEQ START WITH 1033477
+/
+
+INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.nextval, '02/03/1972','Stewart A. Bryson',to_date('01/01/2006','mm/dd/yyyy'),to_date('01/31/2006','mm/dd/yyyy'),'N')
+/
+
+INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart B. Bryson',to_date('02/01/2006','mm/dd/yyyy'),to_date('02/28/2006','mm/dd/yyyy'),'N')
+/
+
+INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart C. Bryson',to_date('03/01/2006','mm/dd/yyyy'),to_date('03/30/2006','mm/dd/yyyy'),'Y')
+/
+
+INSERT INTO test_stg VALUES (test_natkey_seq.currval, '02/04/1972','Stewart W. Bryson',sysdate)
+/
+SELECT * FROM TEST_DIM
+/
