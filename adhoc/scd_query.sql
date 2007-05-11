f4948a7da9c22266ad57dcef4fbb80099a40ce0b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-11 14:02:20 +0000

    Newest query for SCD using MODEL.

---
+++ adhoc/scd_query.sql
@@ -0,0 +1,54 @@
+SELECT CASE test_key WHEN -1 THEN test_dim_seq.nextval ELSE test_key END test_key,
+       nat_key,
+       birthdate,
+       name,
+       effect_start_dt,
+       effect_end_dt,
+       new_current_ind current_ind
+  FROM ( SELECT *
+           FROM (SELECT nat_key,
+                        'S' source,
+			nvl(effect_start_dt,SYSDATE) effect_start_dt,
+			to_date('12/31/9999','mm/dd/yyyy') effect_end_dt,
+                        'N' current_ind,
+                        'Y' new_current_ind,
+                        -1 test_key,
+                        birthdate,
+                        name,
+                        'Y' include
+                   FROM test_stg
+                        UNION
+                 SELECT nat_key,
+                        'D' source,
+			effect_start_dt,
+			effect_end_dt,
+                        current_ind,
+                        current_ind new_current_ind,
+                        test_key,
+                        birthdate,
+                        name,
+                        'Y' include
+                   FROM test_dim)
+          MODEL partition BY ( nat_key)
+                dimension BY ( source,
+                               current_ind)
+                measures ( test_key,
+                           birthdate,
+                           name,
+                           include,
+			   effect_start_dt,
+			   effect_end_dt,
+                           new_current_ind)
+                UNIQUE single reference
+                rules ( -- SET the include flag to 'Y' or 'N' to determine whether to include the row from STG table
+                        include['S','N'] = CASE WHEN name['S','N'] = name['D','Y'] THEN 'N' ELSE 'Y' END,
+                        -- SET the CURRENT_IND flag of the current record to 'Y' or 'N'
+                        effect_end_dt['D','Y'] = CASE WHEN name['S','N'] = name['D','Y'] THEN effect_end_dt['S','N'] ELSE effect_start_dt['S','N'] END,
+                        -- SET the effect dates
+                        new_current_ind['D','Y'] = CASE WHEN name['S','N'] = name['D','Y'] THEN 'Y' ELSE 'N' END,
+                        -- SET the TYPE 1 attribute to the new value for all records
+                        birthdate['D',ANY] = birthdate['S','N']
+                      )
+                ORDER BY source,name)
+ WHERE include='Y'
+/
