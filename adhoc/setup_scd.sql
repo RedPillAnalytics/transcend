62d4da0e118d8da06089922931fe8417376948b5
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-11 14:10:34 +0000

    Newest query for SCD using MODEL.

--- adhoc/setup_scd.sql
+++ adhoc/setup_scd.sql
@@ -6,8 +6,8 @@ CREATE TABLE TEST_DIM
         NAT_KEY NUMBER,
         BIRTHDATE DATE,
         NAME VARCHAR2(50),
-        EFFECTIVE_START_DT DATE,
-        EFFECTIVE_END_DT DATE,
+        EFFECT_START_DT DATE,
+        EFFECT_END_DT DATE,
         CURRENT_IND VARCHAR2(1)
    )
 /
@@ -19,7 +19,7 @@ CREATE TABLE TEST_STG
    (    NAT_KEY NUMBER,
         BIRTHDATE DATE,
         NAME VARCHAR2(50),
-        EFFECTIVE_START_DT DATE
+        EFFECT_START_DT DATE
    )
 /
 
@@ -36,10 +36,10 @@ CREATE SEQUENCE TEST_NATKEY_SEQ START WITH 1033477
 INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.nextval, '02/03/1972','Stewart A. Bryson',to_date('01/01/2006','mm/dd/yyyy'),to_date('01/31/2006','mm/dd/yyyy'),'N')
 /
 
-INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart B. Bryson',to_date('02/01/2006','mm/dd/yyyy'),to_date('02/28/2006','mm/dd/yyyy'),'N')
+INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart B. Bryson',to_date('01/31/2006','mm/dd/yyyy'),to_date('02/28/2006','mm/dd/yyyy'),'N')
 /
 
-INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart C. Bryson',to_date('03/01/2006','mm/dd/yyyy'),to_date('03/30/2006','mm/dd/yyyy'),'Y')
+INSERT INTO test_dim VALUES (test_dim_seq.nextval, test_natkey_seq.currval, '02/03/1972','Stewart C. Bryson',to_date('02/28/2006','mm/dd/yyyy'),to_date('12/31/9999','mm/dd/yyyy'),'Y')
 /
 
 INSERT INTO test_stg VALUES (test_natkey_seq.currval, '02/04/1972','Stewart W. Bryson',sysdate)
