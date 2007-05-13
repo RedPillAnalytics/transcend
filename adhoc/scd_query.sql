1abe79bdd11921365d6d9fa19f4c62c5be131761
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-13 20:50:53 +0000

    This version handles multiple records for the same natural key in the staging table.
    This version represents a complete solution for any SCD.

--- adhoc/scd_query.sql
+++ adhoc/scd_query.sql
@@ -1,80 +1,70 @@
-SELECT -- the staging records don't have a surrogate key yet
-       -- the '-1' is a constant for all records from the staging table
-       CASE test_key 
+SELECT CASE test_key 
          WHEN -1 
             THEN test_dim_seq.nextval 
             ELSE test_key 
        END test_key,
-       -- now just select out the rest of the records
        nat_key,
        birthdate,
        name,
        zip,
        effect_start_dt,
-       effect_end_dt,
-       new_current_ind current_ind
-  FROM ( SELECT *
-           FROM (SELECT nat_key,
-                        'S' source,
-			nvl(effect_start_dt,SYSDATE) effect_start_dt,
-			to_date('12/31/9999','mm/dd/yyyy') effect_end_dt,
-                        'N' current_ind,
-                        'Y' new_current_ind,
-                        -1 test_key,
-                        birthdate,
-                        name,
-			zip,
-                        'Y' include
-                   FROM test_stg
-                        UNION
-                 SELECT nat_key,
-                        'D' source,
-			effect_start_dt,
-			effect_end_dt,
-                        current_ind,
-                        current_ind new_current_ind,
-                        test_key,
-                        birthdate,
-                        name,
-			zip,
-                        'Y' include
-                   FROM test_dim)
-          MODEL partition BY ( nat_key)
-                dimension BY ( source,
-                               current_ind)
-                measures ( test_key,
-                           birthdate,
-                           name,
-			   zip,
-                           include,
-			   effect_start_dt,
-			   effect_end_dt,
-                           new_current_ind)
-                UNIQUE single reference
-                rules ( -- SET the include flag to 'Y' or 'N' to determine whether to include the row from STG table
-                        include['S','N'] = CASE 
-			                        WHEN name['S','N'] <> name['D','Y'] 
-		                                  OR zip['S','N'] <> zip['D','Y'] 
-			                        THEN 'Y' 
-			                        ELSE 'N' 
-					   END,
-                        -- SET the CURRENT_IND flag of the current record to 'Y' or 'N'
-                        effect_end_dt['D','Y'] = CASE 
-			                              WHEN name['S','N'] <> name['D','Y'] 
-			                                OR zip['S','N'] <> zip['D','Y'] 
-			                              THEN effect_start_dt['S','N'] 
-			                              ELSE effect_end_dt['S','N'] 
-						 END,
-                        -- SET the effect dates
-                        new_current_ind['D','Y'] = CASE 
-			                              WHEN name['S','N'] <> name['D','Y'] 
-			                                OR zip['S','N'] <> zip['D','Y'] 
-			                              THEN 'N' 
-			                              ELSE 'Y' 
-						 END,
-                        -- SET the TYPE 1 attribute to the new value for all records
-                        birthdate['D',ANY] = birthdate['S','N']
-                      )
-          ORDER BY nat_key,source,effect_start_dt)
+       nvl(lead(effect_start_dt) OVER (partition BY nat_key ORDER BY effect_start_dt), to_date('12/31/9999','mm/dd/yyyy')) effect_end_dt,
+       CASE MAX(effect_start_dt) OVER (partition BY nat_key)
+       WHEN effect_start_dt
+       THEN 'Y'
+       ELSE 'N'
+       END current_ind
+  FROM (SELECT * FROM (
+SELECT complete_data.*,
+       CASE MAX(effect_start_dt) OVER (partition BY nat_key, source)
+       WHEN effect_start_dt THEN 'Y'
+       ELSE 'N'
+       END max_source_dt_ind,
+       'Y' include
+  FROM (SELECT nat_key,
+               effect_start_dt,
+               -1 test_key,
+               birthdate,
+               name,
+               zip,
+               'S' source
+          FROM test_stg
+               UNION
+        SELECT nat_key,
+               effect_start_dt,
+               test_key,
+               birthdate,
+               name,
+               zip,
+               'D' source
+          FROM test_dim) complete_data)
+ MODEL partition BY ( nat_key)
+       dimension BY ( source,
+                      max_source_dt_ind
+                    )
+       measures ( test_key,
+                  birthdate,
+                  name,
+                  zip,
+                  include,
+                  effect_start_dt)
+       UNIQUE single reference
+       rules ( -- SET the include flag to 'Y' or 'N' to determine whether to include the row from STG table
+               include['S','Y'] = CASE
+               WHEN name['S','Y'] <> name['D','Y']
+               OR zip['S','Y'] <> zip['D','Y']
+               THEN 'Y'
+               ELSE 'N'
+               END,
+               include['S','N'] = CASE
+               WHEN name['S','N'] <> name['S','Y']
+               OR zip['S','N'] <> zip['S','Y']
+               THEN 'Y'
+               ELSE 'N'
+               END,
+               -- SET the TYPE 1 attribute to the new value for all records
+               birthdate['D',ANY] = birthdate['S','Y']
+             )
+       ) include_data
  WHERE include='Y'
 /
