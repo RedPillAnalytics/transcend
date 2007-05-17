a3bf54f677f062dd428bb37956a1831a747b3bf3
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-17 17:52:27 +0000

    Rewrote using simple anayltics. Now, each record from the staging table is evaluated for inclusion on whether the type2 attributes differ from the previous record.

--- adhoc/scd_query.sql
+++ adhoc/scd_query.sql
@@ -8,71 +8,64 @@ SELECT CASE test_key
        name,
        zip,
        effect_start_dt,
-       nvl(lead(effect_start_dt) OVER 
-	    (partition BY nat_key ORDER BY effect_start_dt), to_date('12/31/9999','mm/dd/yyyy')) effect_end_dt,
+       -- effective end date is always based on EFFECTIVE_START_DT of proceeding record
+       nvl( lead(effect_start_dt) OVER 
+	    ( partition BY nat_key 
+	      ORDER BY effect_start_dt), to_date('12/31/9999','mm/dd/yyyy')) effect_end_dt,
        CASE MAX(effect_start_dt) 
-          OVER (partition BY nat_key)
+       OVER (partition BY nat_key)
        WHEN effect_start_dt
        THEN 'Y'
        ELSE 'N'
        END current_ind
-  FROM (SELECT * 
-	  FROM ( SELECT complete_data.*,
-			-- SET the max record based on dt for both source data and dimension data
-			CASE MAX(effect_start_dt) OVER (partition BY nat_key, source)
-			WHEN effect_start_dt THEN 'Y'
-			ELSE 'N'
-			END max_source_dt_ind,
-			'Y' include
-		   FROM (SELECT nat_key,
-				effect_start_dt,
-				-- FOR now, use a -1 for the surrogate key
-				-- selecting from a sequence is not allowed inside analytics statements
-				-- it'll be cased later
-				-1 test_key,
-				birthdate,
-				name,
-				zip,
-				-- need to identify where each record came from 
-				'S' source
-			   FROM test_stg
-				UNION
-			 SELECT nat_key,
-				effect_start_dt,
-				test_key,
-				birthdate,
-				name,
-				zip,
-				-- need to identify where each record came from 
-				'D' source
-			   FROM test_dim) complete_data)
-	 MODEL partition BY ( nat_key)
-	       -- dimension by the source of the data, and whether each record is max within that source
-	       dimension BY ( source,
-			      max_source_dt_ind
-			    )
-	       -- measures have to include everything not in partition and dimension expressions
-	       measures ( test_key,
-			  birthdate,
-			  name,
-			  zip,
-			  include,
-			  effect_start_dt)
-	       UNIQUE single reference
-	       rules ( -- SET the include flag to 'Y' or 'N' to determine whether to keep the MAX value from source
-		       -- decision is made based on comparing all the type2 attributes
-		       include['S','Y'] = CASE
-		       WHEN name['S','Y'] <> name['D','Y']
-		       OR zip['S','Y'] <> zip['D','Y']
-		       THEN 'Y'
-		       ELSE 'N'
-		       END,
-		       -- SET the TYPE 1 attribute to the value of the max source record if it exists
-		       -- otherwise set it to the value for the max dimension record (which it should have been to begin with)
-		       -- there needs to be a rule here for every type1 attribute
-		       birthdate[ANY,ANY] = CASE WHEN birthdate['S','Y'] IS present THEN birthdate['S','Y'] ELSE birthdate['D','Y'] END 
-		     )
-       ) include_data
-       -- ONLY get records that we've decided to include
+  FROM (SELECT test_key,
+	       nat_key,
+	       -- will need a LAST_VALUE function for each Type1 attribute
+	       -- notice the LAST_VALUE looks at all records, not just staging records
+	       -- if the staging table is filled with records predating the last dimension record,
+	       -- then we would want the value of the dimension record to pervade
+	       -- we also do this prior to excluding records
+	       -- that's because we may want a type1 attribute from an excluded record
+	       last_value(birthdate) OVER ( partition BY nat_key 
+					    ORDER BY effect_start_dt 
+					    ROWS BETWEEN unbounded preceding AND unbounded following) birthdate,
+	       name,
+	       zip,
+	       effect_start_dt,
+	       source,
+	       -- we now exclude records based on what the value is directly preceeding the record
+	       -- the list of requirements in the CASE statement needs to include all Type 2 attributes
+	       CASE 
+	       WHEN 
+	       ( zip = lag(zip) OVER (partition BY nat_key ORDER BY effect_start_dt) 
+		 and name = lag(name) OVER (partition BY nat_key ORDER BY effect_start_dt))
+	       -- only consider excluding staging records... never dimension records
+	       AND source='S'
+	       THEN 'N'
+	       ELSE 'Y'
+	       END include
+	  FROM (SELECT nat_key,
+		       effect_start_dt,
+		       -- FOR now, use a -1 for the surrogate key
+		       -- selecting from a sequence is not allowed inside analytics statements
+		       -- it'll be cased later
+		       -1 test_key,
+		       birthdate,
+		       name,
+		       zip,
+		       -- need to identify where each record came from 
+		       'S' source
+		  FROM test_stg
+		       UNION
+		SELECT nat_key,
+		       effect_start_dt,
+		       test_key,
+		       birthdate,
+		       name,
+		       zip,
+		       -- need to identify where each record came from 
+		       'D' source
+		  FROM test_dim)
+	 ORDER BY nat_key, effect_start_dt)
  WHERE include='Y'
 /
