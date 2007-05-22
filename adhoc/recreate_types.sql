6fb3ea7102bb0c7dbc7209fd77a7a788a23cd8f6
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-05-22 14:21:05 +0000

    belongs in the adhoc directory

---
+++ adhoc/recreate_types.sql
@@ -0,0 +1,15 @@
+DROP TYPE tdinc.feed;
+DROP TYPE tdinc.extract;
+DROP TYPE tdinc.fhconf;
+DROP TYPE tdinc.notify;
+
+@BASETYPE.tps
+@BASETYPE.tpb
+@NOTIFY.tps
+@NOTIFY.tpb
+@FHCONF.tps
+@FHCONF.tpb
+@FEED.tps
+@FEED.tpb
+@EXTRACT.tps
+@EXTRACT.tpb
