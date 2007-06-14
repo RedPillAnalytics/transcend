1ce858640887e6e61a4ff30b2b5d1a8d34bee52f
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-14 18:42:19 +0000

    Made changes in the Type 2 case statement to make the process re-runnable, and to also simply.

--- adhoc/scd_query.sql
+++ adhoc/scd_query.sql
@@ -19,7 +19,7 @@ SELECT CASE test_key
        WHEN effect_start_dt
        THEN 'Y'
        ELSE 'N'
-       END current_ind 
+       END current_ind
   FROM (SELECT test_key,
 	       nat_key,
 	       -- will need a LAST_VALUE function for each Type1 attribute
@@ -35,23 +35,30 @@ SELECT CASE test_key
 	       zip,
 	       zip_plus4,
 	       effect_start_dt,
-	       -- we now exclude records based on what the value is directly preceeding the record
-	       -- the list of requirements in the CASE statement needs to include all Type 2 attributes
-	       -- the coaelesce helps ensure that a NULL = NULL issue does not slip through
+	       -- now do a series of comparisons of each record to see whether the value of the INCLUDE column is 'Y' or 'N'
 	       CASE
-	       WHEN zip <> LAG(zip) OVER (partition BY nat_key ORDER BY effect_start_dt)
+	       -- if a record is an existing DIM record, then we know we want to include it
+	       WHEN test_key <> -1
 	       THEN 'Y'
-	       WHEN zip_plus4 <> LAG(zip_plus4) OVER (partition BY nat_key ORDER BY effect_start_dt)
-	       THEN 'Y'
-	       WHEN name <> LAG(name) OVER (partition BY nat_key ORDER BY effect_start_dt)
-	       THEN 'Y'
-	       WHEN effect_start_dt=LAG(effect_start_dt) OVER (partition BY nat_key ORDER BY effect_start_dt)
+	       -- if we ever have a record where the effective date and expiry date are the same, they should be excluded
+	       -- this situation makes no logical sense, and usually wouldn't happen
+	       -- this can sometimes occur on a rerun of a dimensional load
+	       WHEN effect_start_dt=LAG(effect_start_dt) OVER (partition BY nat_key ORDER BY effect_start_dt,test_key desc)
 	       THEN 'N'
-	       ELSE
-	       CASE test_key
-	       WHEN -1 THEN 'N'
-	       ELSE 'Y'
-	       END
+	       -- we now exclude records based on comparing all type2 attributes with the preceeding record
+	       -- if any of these attributes are different, then the record would get excluded
+	       -- want to include any record that has a change in a type2 field compared to the previous record
+	       -- have to do nvl's based on datatypes of both operands to make sure that we are not catching an 'unknown'
+	       -- nvl'ing to values that don't naturally exist in the data
+	       WHEN nvl(zip,-.01) <> nvl(LAG(zip) OVER (partition BY nat_key ORDER BY effect_start_dt),-.01)
+	       THEN 'Y'
+	       WHEN nvl(zip_plus4,-.01) <> nvl(LAG(zip_plus4) OVER (partition BY nat_key ORDER BY effect_start_dt),-.01)
+	       THEN 'Y'
+	       WHEN nvl(name,-.01) <> nvl(LAG(name) OVER (partition BY nat_key ORDER BY effect_start_dt),-.01)
+	       THEN 'Y'
+	       -- any record that hasn't been caught yet has no type2 changes
+	       -- that record should not be included
+	       ELSE 'N'
 	       END include
 	  FROM (SELECT nat_key,
 		       effect_start_dt,
