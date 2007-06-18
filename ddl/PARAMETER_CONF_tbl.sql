127980a3460a894a6e7c6a0cc83c12af4b675939
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-18 16:09:45 +0000

    Removed schema prefix.

--- ddl/PARAMETER_CONF_tbl.sql
+++ ddl/PARAMETER_CONF_tbl.sql
@@ -1,10 +1,10 @@
-DROP TABLE tdinc.parameter_conf CASCADE CONSTRAINTS purge
+DROP TABLE parameter_conf CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE tdinc.parameter_conf_seq
+DROP SEQUENCE parameter_conf_seq
 /
 
-CREATE TABLE tdinc.parameter_conf
+CREATE TABLE parameter_conf
        ( parameter_id	NUMBER NOT NULL,
 	 module 	VARCHAR2(48) NOT NULL,
 	 action		VARCHAR2(32),
@@ -17,22 +17,22 @@ CREATE TABLE tdinc.parameter_conf
 )
 /
 
-COMMENT ON TABLE tdinc.parameter_conf IS 'table to hold parameters for the entire framework';
+COMMENT ON TABLE parameter_conf IS 'table to hold parameters for the entire framework';
 
-ALTER TABLE tdinc.parameter_conf ADD (
+ALTER TABLE parameter_conf ADD (
   CONSTRAINT parameter_conf_pk
  PRIMARY KEY
  (parameter_id)
     USING INDEX)
 /
 
-CREATE SEQUENCE tdinc.parameter_conf_seq
+CREATE SEQUENCE parameter_conf_seq
 /
 
-ALTER TABLE tdinc.parameter_conf ADD CONSTRAINT parameter_conf_uk1 UNIQUE (name) USING INDEX;
-ALTER TABLE tdinc.parameter_conf ADD CONSTRAINT parameter_conf_ck1 CHECK (REGEXP_LIKE(name,'[[:lower:]]+'));
-ALTER TABLE tdinc.parameter_conf ADD CONSTRAINT parameter_conf_ck2 CHECK (REGEXP_LIKE(value,'([[:lower:]]|[[:digit:]])+'));
+ALTER TABLE parameter_conf ADD CONSTRAINT parameter_conf_uk1 UNIQUE (name) USING INDEX;
+ALTER TABLE parameter_conf ADD CONSTRAINT parameter_conf_ck1 CHECK (REGEXP_LIKE(name,'[[:lower:]]+'));
+ALTER TABLE parameter_conf ADD CONSTRAINT parameter_conf_ck2 CHECK (REGEXP_LIKE(value,'([[:lower:]]|[[:digit:]])+'));
 
-INSERT INTO tdinc.parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (tdinc.parameter_conf_seq.nextval,'system','runmode','runtime',sys_context('USERENV','SESSION_USER'),SYSDATE);
-INSERT INTO tdinc.parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (tdinc.parameter_conf_seq.nextval,'system','registration','register',sys_context('USERENV','SESSION_USER'),SYSDATE);
-INSERT INTO tdinc.parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (tdinc.parameter_conf_seq.nextval,'system','logging_level','2',sys_context('USERENV','SESSION_USER'),SYSDATE);
+INSERT INTO parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (parameter_conf_seq.nextval,'system','runmode','runtime',sys_context('USERENV','SESSION_USER'),SYSDATE);
+INSERT INTO parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (parameter_conf_seq.nextval,'system','registration','register',sys_context('USERENV','SESSION_USER'),SYSDATE);
+INSERT INTO parameter_conf (parameter_id,module,name,value,created_user,created_dt) VALUES (parameter_conf_seq.nextval,'system','logging_level','2',sys_context('USERENV','SESSION_USER'),SYSDATE);
