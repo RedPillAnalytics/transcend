135c75e6e68d151bc84adaec9862752bbb23a92c
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-18 14:41:49 +0000

    Removed natural key sequences and just hard-coded them.
    Added additional columns for more test cases.

--- adhoc/setup_scd.sql
+++ adhoc/setup_scd.sql
@@ -1,6 +1,4 @@
-
-COL name format a20
-DROP TABLE test_dim
+DROP TABLE test_dim purge
 /
 
 CREATE TABLE test_dim
@@ -9,13 +7,14 @@ CREATE TABLE test_dim
          birthdate DATE,
 	 name VARCHAR2(50),
 	 zip NUMBER,
+	 zip_plus4 NUMBER,
          effect_start_dt DATE,
          effect_end_dt DATE,
          current_ind VARCHAR2(1)
        )
 /
 
-DROP TABLE test_stg
+DROP TABLE test_stg purge
 /
 
 CREATE TABLE test_stg
@@ -23,6 +22,7 @@ CREATE TABLE test_stg
          birthdate DATE,
 	 name VARCHAR2(50),
 	 zip NUMBER,
+	 zip_plus4 NUMBER,
          effect_start_dt DATE
        )
 /
@@ -32,48 +32,44 @@ DROP SEQUENCE test_dim_seq
 CREATE SEQUENCE test_dim_seq START WITH 21
 /
 
-DROP SEQUENCE test_natkey_seq
-/
-CREATE SEQUENCE test_natkey_seq START WITH 1033477
-/
-
 INSERT INTO test_dim VALUES 
-       (test_dim_seq.nextval, test_natkey_seq.nextval, '01/03/1975','John Smith',30328,
+       (test_dim_seq.nextval, 1033477, '01/03/1975','John Smith',30328,NULL,
 	 to_date('01/01/2006','mm/dd/yyyy'),to_date('01/31/2006','mm/dd/yyyy'),'N')
 /
 
 INSERT INTO test_dim VALUES 
-       (test_dim_seq.nextval, test_natkey_seq.currval, '01/03/1975','John A. Smith',30075,
+       (test_dim_seq.nextval, 1033477, '01/03/1975','John A. Smith',30075,NULL,
 	 to_date('01/31/2006','mm/dd/yyyy'),to_date('02/28/2006','mm/dd/yyyy'),'N')
 /
 
 INSERT INTO test_dim VALUES 
-       (test_dim_seq.nextval, test_natkey_seq.currval, '01/03/1975','John D. Smith',30075,
+       (test_dim_seq.nextval, 1033477, '01/03/1975','John D. Smith',30075,NULL,
 	 to_date('02/28/2006','mm/dd/yyyy'),to_date('12/31/9999','mm/dd/yyyy'),'Y')
 /
 
 INSERT INTO test_stg VALUES 
-       (test_natkey_seq.currval, '01/03/1975', 'John H. Smith', 30066, 
+       (1033477, '01/03/1975', 'John H. Smith', 30066,NULL,
 	 to_date('03/04/2006','mm/dd/yyyy'))
 /
 
 INSERT INTO test_stg VALUES 
-       (test_natkey_seq.currval, '01/04/1975', 'John H. Smith', 30066, 
-	 to_date('03/09/2006','mm/dd/yyyy'))
+       (1033477, '01/04/1975', 'John H. Smith', 30066,3333,
+	 to_date('03/10/2006','mm/dd/yyyy'))
 /
 
+
 INSERT INTO test_dim VALUES 
-       (test_dim_seq.nextval, test_natkey_seq.nextval, '07/26/1975','Jane S. Smith',30328,
+       (test_dim_seq.nextval, 1039242, '07/26/1975','Jane S. Smith',30328,NULL,
 	 to_date('01/01/2006','mm/dd/yyyy'),to_date('12/31/9999','mm/dd/yyyy'),'Y')
 /
 
 INSERT INTO test_stg VALUES 
-       (test_natkey_seq.currval, '09/26/1980','Jane B. Smtih',30075,
+       (1039242, '09/26/1980','Jane B. Smith',NULL,null,
 	 to_date('03/04/2006','mm/dd/yyyy'))
 /
 
 INSERT INTO test_stg VALUES 
-(test_natkey_seq.currval, '08/26/1980','Jane W. Smith',30066,SYSDATE)
+       (1039242, '09/26/1980','Jane B. Smith',NULL,NULL,SYSDATE)
 /
 
 SELECT * FROM test_dim
@@ -97,3 +93,5 @@ INSERT INTO test_fact VALUES (22, 3, 22.14);
 INSERT INTO test_fact VALUES (23, 12, 117.55);
 
 SELECT * FROM test_fact;
+
+COMMIT;
