f8d797bedbdd418897aae2fb1d6a56c37e792880
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-18 16:09:57 +0000

    Removed schema prefix.

--- ddl/PARTNAME_tbl.sql
+++ ddl/PARTNAME_tbl.sql
@@ -1,7 +1,7 @@
-DROP TABLE tdinc.partname purge
+DROP TABLE partname purge
 /
 
-CREATE global TEMPORARY TABLE tdinc.partname
+CREATE global TEMPORARY TABLE partname
 ( partition_name VARCHAR2(30)
 )
 ON COMMIT DELETE ROWS
