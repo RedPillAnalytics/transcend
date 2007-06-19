4e45fa0df7702bdecf0e71bdeab63a2bb385d3ba
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-19 18:00:25 +0000

    Removed 'ACTION' column.

--- ddl/LOGGING_CONF_tbl.sql
+++ ddl/LOGGING_CONF_tbl.sql
@@ -8,7 +8,6 @@ CREATE TABLE logging_conf
        ( logging_id	  NUMBER NOT NULL,
 	 logging_level    NUMBER not NULL,
 	 debug_level 	  NUMBER NOT NULL,
-	 action 	  VARCHAR2(32) NOT NULL,
 	 module 	  VARCHAR2(48) NOT NULL,
 	 created_user     VARCHAR2(30) DEFAULT sys_context('USERENV','SESSION_USER') NOT NULL,
 	 created_dt       DATE DEFAULT SYSDATE NOT NULL,
@@ -26,24 +25,3 @@ ALTER TABLE logging_conf ADD (
 
 CREATE SEQUENCE logging_conf_seq
 /
-
-INSERT INTO logging_conf
-       ( logging_id, 
-	 logging_level, 
-	 debug_level, 
-	 action, 
-	 module, 
-	 created_user, 
-	 created_dt, 
-	 modified_user, 
-	 modified_dt
-       )
-       VALUES ( logging_conf_seq.nextval,
-		2,
-		4,
-		'default',
-		'default',
-		sys_context('USERENV','SESSION_USER'),
-		SYSDATE,
-		NULL,
-		NULL);
