2221eb1c865d6566202a02cdb9da5237467073b5
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-06-26 22:33:17 +0000

    Changed an error code in the error table, and modified the call to that error code.

--- ddl/ERR_CD_tbl.sql
+++ ddl/ERR_CD_tbl.sql
@@ -28,7 +28,7 @@ INSERT INTO err_cd (code,name,message) VALUES (-20010, 'file_too_big','File size
 INSERT INTO err_cd (code,name,message) VALUES (-20011, 'file_too_small','File size smaller than MAX_BYTES paramter');
 INSERT INTO err_cd (code,name,message) VALUES (-20012, 'notify_method_invalid','The notification method is not valid');
 INSERT INTO err_cd (code,name,message) VALUES (-20013, 'no_files_found','No files found for this configuration');
-INSERT INTO err_cd (code,name,message) VALUES (-20015, 'no_ext_tab','External table in this configuration is non-existent');
+INSERT INTO err_cd (code,name,message) VALUES (-20015, 'no_tab','The specified table does not exist');
 INSERT INTO err_cd (code,name,message) VALUES (-20016, 'no_ext_files','There are no files found for this external table');
 INSERT INTO err_cd (code,name,message) VALUES (-20017, 'reject_limit_exceeded','The external table reject limit was exceeded');
 INSERT INTO err_cd (code,name,message) VALUES (-20018, 'ext_file_missing','The physical file specified by the external table LOCATION does not exist');
