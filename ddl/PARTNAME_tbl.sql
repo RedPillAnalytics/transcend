a13a50afe62bad87c8b9632e68f620686ad24807
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-08 03:24:35 +0000

    Removed the HIGH_VALUE column from PartName.

--- ddl/PARTNAME_tbl.sql
+++ ddl/PARTNAME_tbl.sql
@@ -2,7 +2,10 @@ DROP TABLE partname purge
 /
 
 CREATE global TEMPORARY TABLE partname
-( partition_name VARCHAR2(30)
+( table_owner VARCHAR2(30),
+  table_name VARCHAR2(30),
+  partition_name VARCHAR2(30),
+  partition_position NUMBER
 )
 ON COMMIT DELETE ROWS
 /
