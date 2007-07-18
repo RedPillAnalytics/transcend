240bdfbc5ea40bb29a5e13afe8f5fa4e88d0bc3c
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-18 22:52:33 +0000

    Ticket #22
    Propogated the &schema variable throughout to the role names to make roles associated with the different repositories.

--- ddl/FILEHUB_OBJ_DETAIL_tbl.sql
+++ ddl/FILEHUB_OBJ_DETAIL_tbl.sql
@@ -39,11 +39,15 @@ COMMENT ON COLUMN filehub_obj_detail.num_lines IS 'The number of lines at the OS
 COMMENT ON COLUMN filehub_obj_detail.percent_diff IS 'Percentage difference between the two.';
 COMMENT ON COLUMN filehub_obj_detail.session_id IS 'SESSION_ID of the processing session.';
 
-GRANT SELECT ON filehub_obj_detail TO td_sel;
-/
-
 DROP SEQUENCE filehub_obj_detail_seq
 /
 
 CREATE SEQUENCE filehub_obj_detail_seq
+/
+
+GRANT SELECT ON filehub_obj_detail TO td_sel_&schema
+/
+GRANT SELECT,UPDATE,DELETE,INSERT ON filehub_obj_detail TO td_sel_&schema
+/
+GRANT SELECT ON filehub_obj_detail_seq TO td_sel_&SCHEMA
 /
