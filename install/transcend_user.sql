348457980b03ab090f2c8276da620eca8a7a8b23
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-24 14:17:45 +0000

    Ticket #22
    Modified to use the synonym scripts created at [744].

--- install/transcend_user.sql
+++ install/transcend_user.sql
@@ -1,25 +1,12 @@
 SET serveroutput on size unlimited
 SET echo off
 
-ACCEPT repo_user char default 'TDINC_USER' prompt 'Username to configure as Transcend repository user [TDINC_USER]:
-ACCEPT repo_schema char default 'TDINC' prompt 'Schema containing Transcend repository for this user [TDINC]:
+ACCEPT repo_user char default 'TDREP_USER' prompt 'Username to configure as Transcend repository user [TDREP_USER]':
+ACCEPT repo_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [TDREP]':
+ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [TDREP]':
 
-ALTER USER &tab_schema DEFAULT TABLESPACE &tablespace;
-ALTER SESSION SET current_schema=&tab_schema;
+-- create synonyms from the user to the repository
+@@rep_syns &repo_user &repo_schema
 
-SET echo on
-
---first create framework tables
-CREATE SYNONYM &repo_user..COUNT_TABLE FOR &repo_schema..COUNT_TABLE;
-CREATE SYNONYM &repo_user..DIR_LIST FOR &repo_schema..DIR_LIST;
-CREATE SYNONYM &repo_user..ERR_CD FOR &repo_schema..ERR_CD;
-CREATE SYNONYM &repo_user..FILEHUB_CONF FOR &repo_schema..FILEHUB_CONF;
-CREATE SYNONYM &repo_user..FILEHUB_DETAIL FOR &repo_schema..FILEHUB_DETAIL;
-CREATE SYNONYM &repo_user..FILEHUB_OBJ_DETAIL FOR &repo_schema..FILEHUB_OBJ_DETAIL;
-CREATE SYNONYM &repo_user..LOGGING_CONF FOR &repo_schema..LOGGING_CONF;
-CREATE SYNONYM &repo_user..LOG_TABLE FOR &repo_schema..LOG_TABLE;
-CREATE SYNONYM &repo_user..NOTIFY_CONF FOR &repo_schema..NOTIFY_CONF;
-CREATE SYNONYM &repo_user..PARTNAME FOR &repo_schema..PARTNAME;
-CREATE SYNONYM &repo_user..REGISTRATION_CONF FOR &repo_schema..REGISTRATION_CONF;
-CREATE SYNONYM &repo_user..RUNMODE_CONF FOR &repo_schema..RUNMODE_CONF;
-CREATE SYNONYM &repo_user..PARAMETER_CONF FOR &repo_schema..PARAMETER_CONF;
+-- create synonyms from the user to the application
+@@app_syns &repo_user &app_schema
