1a1ca34320f98b6be4fc5e312e172128e1f36cab
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-24 04:02:11 +0000

    Ticket #22
    Split out aspects of the installation into seperate pieces to allow for repository management.

--- install/transcend_repository.sql
+++ install/transcend_repository.sql
@@ -1,44 +1,22 @@
-SET serveroutput on size unlimited
-SET echo off
-
-ACCEPT schema char default 'TDINC' prompt 'Schema to use for Transcend repository [TDINC]: '
-ACCEPT tablespace char default 'TDINC' prompt 'Tablespace to use for Transcend repository [TDINC]: '
-
-CREATE ROLE td_sel_&schema;
-CREATE ROLE td_adm_&schema;
-
-VARIABLE old_tbspace char(30)
-BEGIN
-   SELECT default_tablespace
-     INTO :old_tbspace
-     FROM dba_users
-    WHERE username=upper('&tab_schema');
-END;
-/
-
-ALTER USER &schema DEFAULT TABLESPACE &tablespace;
-ALTER SESSION SET current_schema=&schema;
-
-SET echo on
+-- set the correct schema
+ALTER SESSION SET current_schema=&1;
 
 -- create Transcend repository tables
-@../ddl/COUNT_TABLE_tbl.sql
-@../ddl/DIR_LIST_tbl.sql
-@../ddl/ERR_CD_tbl.sql
-@../ddl/FILEHUB_CONF_tbl.sql
-@../ddl/FILEHUB_DETAIL_tbl.sql
-@../ddl/FILEHUB_OBJ_DETAIL_tbl.sql
-@../ddl/LOGGING_CONF_tbl.sql
-@../ddl/LOG_TABLE_tbl.sql
-@../ddl/NOTIFY_CONF_tbl.sql
-@../ddl/PARTNAME_tbl.sql
-@../ddl/REGISTRATION_CONF_tbl.sql
-@../ddl/RUNMODE_CONF_tbl.sql
-@../ddl/PARAMETER_CONF_tbl.sql
+@@../ddl/DIR_LIST_tbl.sql
+@@../ddl/COUNT_TABLE_tbl.sql
+@@../ddl/ERR_CD_tbl.sql
+@@../ddl/FILEHUB_CONF_tbl.sql
+@@../ddl/FILEHUB_DETAIL_tbl.sql
+@@../ddl/FILEHUB_OBJ_DETAIL_tbl.sql
+@@../ddl/LOGGING_CONF_tbl.sql
+@@../ddl/LOG_TABLE_tbl.sql
+@@../ddl/NOTIFY_CONF_tbl.sql
+@@../ddl/PARTNAME_tbl.sql
+@@../ddl/REGISTRATION_CONF_tbl.sql
+@@../ddl/RUNMODE_CONF_tbl.sql
+@@../ddl/PARAMETER_CONF_tbl.sql
 
+-- issue grants to the roles created for this repository
+@@rep_grants &1
+      
 ALTER SESSION SET current_schema=&_USER;
-
-BEGIN
-   EXECUTE IMMEDIATE 'alter user &tab_schema default tablespace '||:old_tbspace;
-END;
-/
