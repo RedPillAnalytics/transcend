91cc9af6221a19c3aacb3de9e842ff1ba08ed3aa
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-24 16:00:27 +0000

    Ticket #22
    Not completed... need to add the PL/SQL to lock the account based on &response.

--- install/transcend_user.sql
+++ install/transcend_user.sql
@@ -1,12 +1,14 @@
 SET serveroutput on size unlimited
 SET echo off
 
-ACCEPT repo_user char default 'TDREP_USER' prompt 'Username to configure as Transcend repository user [TDREP_USER]':
-ACCEPT repo_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [TDREP]':
-ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [TDREP]':
+ACCEPT repo_user char default 'TDREP_USER' prompt 'Username to configure as Transcend repository user [tdrep_user]:'
+ACCEPT repo_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [tdrep]:'
+ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend application for this user [tdrep]:'
 
 -- create synonyms from the user to the repository
 @@rep_syns &repo_user &repo_schema
 
 -- create synonyms from the user to the application
 @@app_syns &repo_user &app_schema
+
+ACCEPT response char default 'yes' prompt 'Do you want to lock the &app_schema schema? [yes]:'
