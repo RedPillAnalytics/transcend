26d7ea845db08794cca4aff94ac50b64392726f0
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 00:44:12 +0000

    Ticket #22
    Modulularized aspects of the install process to make repository installs easier.

---
+++ install/app_syns.sql
@@ -0,0 +1,16 @@
+-- first parameter passed is the synonym schema
+-- second parameter passed is the object schema
+
+DECLARE
+   l_syn_schema VARCHAR2(30) := '&1';
+   l_obj_schema VARCHAR2(30) := '&2';
+BEGIN   
+   IF upper(l_syn_schema) <> upper(l_obj_schema)
+   THEN
+      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.td_fileapi for '||l_obj_schema||'.td_fileapi';
+      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.td_dbapi for '||l_obj_schema||'.td_dbapi';
+      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.td_owbapi for '||l_obj_schema||'.td_owbapi';
+   END IF;
+      
+END;
+/
