057ac4cb2e44c3fc7fde67040fc6e882a63c477b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 18:17:04 +0000

    Ticket #22.
    Added grant of both the repository role and the application role to the repository user.

--- install/install.sql
+++ install/install.sql
@@ -2,6 +2,7 @@ SET serveroutput on size unlimited
 SET echo off
 ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
 SPOOL install_&_DATE..log
+
 -- first get the schema for the Transcend repository (tables) first
 ACCEPT td_rep char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
 -- get the tablespace for the repository
