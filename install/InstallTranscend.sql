068219f27b10e10062397ad0cf99d006ed2fd97f
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 19:50:57 +0000

    Ticket #22.
    Modularized code from main scripts into callable scripts.
    
    Created the CreateTranscendUser.sql and CreateTranscendRepository.sql scripts, which are designed to be called by the user.

--- install/InstallTranscend.sql
+++ install/InstallTranscend.sql
@@ -1,7 +1,7 @@
 SET serveroutput on size unlimited
 SET echo off
 ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
-SPOOL install_&_DATE..log
+SPOOL InstallTranscend_&_DATE..log
 
 -- first get the schema for the Transcend repository (tables) first
 ACCEPT td_rep char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
