59463803af0245d6dd181569f47e1252ffc5f261
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 03:40:18 +0000

    Ticket #22.
    Moving grants around to facilitate multiple repositories.

--- install/transcend_application.sql
+++ install/transcend_application.sql
@@ -1,37 +1,56 @@
-SET echo on
-
 -- create a super role to grant complete power for the entire framework using system privileges
 DROP ROLE &1._sys;
+DROP ROLE &1;
 CREATE ROLE &1._sys;
+CREATE ROLE &1;
 
--- grant full execution rights to the system role
+-- grant full execution rights to the system role as well as the application schema
+-- will have a chance to lock the application schema later
 GRANT CONNECT TO &1._sys;
+GRANT CONNECT TO &1;
 GRANT RESOURCE TO &1._sys;
+GRANT RESOURCE TO &1;
 GRANT ALTER ANY TABLE TO &1._sys;
+GRANT ALTER ANY TABLE TO &1;
 GRANT ALTER SESSION TO &1._sys;
+GRANT ALTER SESSION TO &1;
 GRANT EXECUTE ANY PROCEDURE TO &1._sys;
+GRANT EXECUTE ANY PROCEDURE TO &1;
 GRANT INSERT ANY TABLE TO &1._sys;
+GRANT INSERT ANY TABLE TO &1;
 GRANT SELECT ANY dictionary TO &1._sys;
+GRANT SELECT ANY dictionary TO &1;
 GRANT SELECT ANY TABLE TO &1._sys;
+GRANT SELECT ANY TABLE TO &1;
 GRANT UPDATE ANY TABLE TO &1._sys;
+GRANT UPDATE ANY TABLE TO &1;
 GRANT ALTER ANY INDEX TO &1._sys;
+GRANT ALTER ANY INDEX TO &1;
 GRANT CREATE ANY INDEX TO &1._sys;
+GRANT CREATE ANY INDEX TO &1;
 GRANT DROP ANY INDEX TO &1._sys;
+GRANT DROP ANY INDEX TO &1;
 GRANT DROP ANY TABLE TO &1._sys;
+GRANT DROP ANY TABLE TO &1;
 GRANT CREATE ANY directory TO &1._sys;
+GRANT CREATE ANY directory TO &1;
 GRANT EXECUTE ON sys.utl_mail TO &1._sys;
+GRANT EXECUTE ON sys.utl_mail TO &1;
 
 --java permissions
 EXEC dbms_java.set_output(1000000);
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'execute' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'execute' );
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'read' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'read' );
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'write' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'write' );
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'delete' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.io.FilePermission', '<<ALL FILES>>', 'delete' );
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.lang.RuntimePermission', 'writeFileDescriptor', '' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.lang.RuntimePermission', 'writeFileDescriptor', '' );
+EXEC dbms_java.grant_permission( upper('&1._sys'), 'SYS:java.lang.RuntimePermission', 'readFileDescriptor','' );
 EXEC dbms_java.grant_permission( upper('&1'), 'SYS:java.lang.RuntimePermission', 'readFileDescriptor','' );
-
--- grant the system role to the application schema
-GRANT &1._sys TO &1;
 
 -- set current schema
 ALTER SESSION SET current_schema=&1;
@@ -123,3 +142,16 @@ GRANT EXECUTE ON TD_CONTROL to &1._app;
 GRANT EXECUTE ON TD_OWBAPI to &1._app;
 
 ALTER SESSION SET current_schema=&_USER;
+
+ACCEPT response char default 'yes' prompt 'Do you want to lock the &1? [yes]:'
+
+DECLARE
+   l_response VARCHAR2(30) := lower('&response');
+   l_app_schema VARCHAR2(30) := upper('&1');
+BEGIN   
+   IF l_response = 'yes'
+   THEN
+      EXECUTE IMMEDIATE 'alter user '||l_app_schema||' account lock';
+   END IF;      
+END;
+/
