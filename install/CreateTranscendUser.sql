2e2b28d7c289e154044eaae1e8a0d06418895789
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 19:43:00 +0000

    Ticket #22.
    Modularized code from main scripts into callable scripts.
    
    Created the CreateTranscendUser.sql script, which is designed to be called by the user.

---
+++ install/CreateTranscendUser.sql
@@ -0,0 +1,23 @@
+SET serveroutput on size unlimited
+SET echo off
+
+ACCEPT repo_user char default 'TDREP_USER' prompt 'Username to configure as Transcend repository user [tdrep_user]:'
+ACCEPT repo_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [tdrep]:'
+ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend application for this user [tdrep]:'
+-- lock the application schema if desired
+ACCEPT response char default 'yes' prompt 'Do you want to lock the &1? [yes]:'
+
+-- call the modular script
+@@transcend_user &repo_user &repo_schema &app_schema
+
+
+DECLARE
+   l_response VARCHAR2(30) := lower('&response');
+   l_app_schema VARCHAR2(30) := upper('&app_schema');
+BEGIN   
+   IF l_response = 'yes'
+   THEN
+      EXECUTE IMMEDIATE 'alter user '||l_app_schema||' account lock';
+   END IF;      
+END;
+/
