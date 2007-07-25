0ee02777b272cbe5269935c232be949355572992
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-25 18:17:10 +0000

    Ticket #22.
    Added grant of both the repository role and the application role to the repository user.

--- install/transcend_user.sql
+++ install/transcend_user.sql
@@ -10,3 +10,7 @@ ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend appli
 
 -- create synonyms from the user to the application
 @@app_syns &repo_user &app_schema
+
+-- need to give roles for the repository and the application
+GRANT &repo_schema._adm TO &repo_user;
+GRANT &app_schema._app TO &repo_user;
