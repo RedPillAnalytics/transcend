3122ca6db0c7f9f207a2be8747b410f64f6bb2e3
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 21:01:55 +0000

    Ticket #22
    Renamed variables for more clarity.

--- install/transcend_user.sql
+++ install/transcend_user.sql
@@ -1,17 +1,20 @@
 PROMPT 'Running transcend_user.sql'
 -- &1 IS the repository USER
-DEFINE rep_user = &1
+DEFINE rep_user_tu = &1
 -- &2 IS the repository SCHEMA
-DEFINE rep_schema = &2
+DEFINE rep_schema_tu = &2
 -- &3 IS the application SCHEMA
-DEFINE app_schema =&3
+DEFINE app_schema_tu =&3
+
+-- create the user if it doesn't exist
+@@create_app_user &rep_user_tu
 
 -- create synonyms from the user to the repository
-@@rep_syns &rep_user &rep_schema
+@@rep_syns &rep_user_tu &rep_schema_tu
 
 -- create synonyms from the user to the application
-@@app_syns &rep_user &app_schema
+@@app_syns &rep_user_tu &app_schema_tu
 
 -- need to give roles for the repository and the application
-GRANT &rep_schema._adm TO &rep_user;
-GRANT &app_schema._app TO &rep_user;
+GRANT &rep_schema_tu._adm TO &rep_user_tu;
+GRANT &app_schema_tu._app TO &rep_user_tu;
