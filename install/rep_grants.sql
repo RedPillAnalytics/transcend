24329f6e6d3310ec8887018ff640453e169d7153
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 19:47:12 +0000

    Ticket #22
    Using suffixes to all the variables for the different components.

--- install/rep_grants.sql
+++ install/rep_grants.sql
@@ -1,62 +1,62 @@
 PROMPT 'Running rep_grants.sql'
 -- &1 IS the repository SCHEMA
-DEFINE rep_schema = &1
+DEFINE rep_schema_rg = &1
 
 -- create role for this repository
-DROP ROLE &rep_schema._sel;
-CREATE ROLE &rep_schema._sel;
-DROP ROLE &rep_schema._adm;
-CREATE ROLE &rep_schema._adm;
+DROP ROLE &rep_schema_rg._sel;
+CREATE ROLE &rep_schema_rg._sel;
+DROP ROLE &rep_schema_rg._adm;
+CREATE ROLE &rep_schema_rg._adm;
 
 
-GRANT SELECT ON COUNT_TABLE TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON COUNT_TABLE TO &rep_schema._adm;
+GRANT SELECT ON COUNT_TABLE TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON COUNT_TABLE TO &rep_schema_rg._adm;
 
-GRANT SELECT ON DIR_LIST TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON DIR_LIST TO &rep_schema._adm;
+GRANT SELECT ON DIR_LIST TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON DIR_LIST TO &rep_schema_rg._adm;
 
-GRANT SELECT ON FILEHUB_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_CONF TO &rep_schema._adm;
-GRANT SELECT ON FILEHUB_CONF_SEQ TO &rep_schema._sel;
-GRANT SELECT ON FILEHUB_CONF_SEQ TO &rep_schema._adm;
+GRANT SELECT ON FILEHUB_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON FILEHUB_CONF_SEQ TO &rep_schema_rg._sel;
+GRANT SELECT ON FILEHUB_CONF_SEQ TO &rep_schema_rg._adm;
 
-GRANT SELECT ON FILEHUB_DETAIL TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_DETAIL TO &rep_schema._adm;
-GRANT SELECT ON filehub_detail_seq TO &rep_schema._sel;
-GRANT SELECT ON filehub_detail_seq TO &rep_schema._adm;
+GRANT SELECT ON FILEHUB_DETAIL TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_DETAIL TO &rep_schema_rg._adm;
+GRANT SELECT ON filehub_detail_seq TO &rep_schema_rg._sel;
+GRANT SELECT ON filehub_detail_seq TO &rep_schema_rg._adm;
 
-GRANT SELECT ON FILEHUB_OBJ_DETAIL TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_OBJ_DETAIL TO &rep_schema._adm;
-GRANT SELECT ON filehub_obj_detail_seq TO &rep_schema._sel;
-GRANT SELECT ON filehub_obj_detail_seq TO &rep_schema._adm;
+GRANT SELECT ON FILEHUB_OBJ_DETAIL TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON FILEHUB_OBJ_DETAIL TO &rep_schema_rg._adm;
+GRANT SELECT ON filehub_obj_detail_seq TO &rep_schema_rg._sel;
+GRANT SELECT ON filehub_obj_detail_seq TO &rep_schema_rg._adm;
 
-GRANT SELECT ON LOGGING_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON LOGGING_CONF TO &rep_schema._adm;
-GRANT SELECT ON logging_conf_seq TO &rep_schema._sel;
-GRANT SELECT ON logging_conf_seq TO &rep_schema._adm;
+GRANT SELECT ON LOGGING_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON LOGGING_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON logging_conf_seq TO &rep_schema_rg._sel;
+GRANT SELECT ON logging_conf_seq TO &rep_schema_rg._adm;
 
-GRANT SELECT ON LOG_TABLE TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON LOG_TABLE TO &rep_schema._adm;
+GRANT SELECT ON LOG_TABLE TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON LOG_TABLE TO &rep_schema_rg._adm;
 
-GRANT SELECT ON NOTIFY_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON NOTIFY_CONF TO &rep_schema._adm;
-GRANT SELECT ON notify_conf_seq TO &rep_schema._sel;
-GRANT SELECT ON notify_conf_seq TO &rep_schema._adm;
+GRANT SELECT ON NOTIFY_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON NOTIFY_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON notify_conf_seq TO &rep_schema_rg._sel;
+GRANT SELECT ON notify_conf_seq TO &rep_schema_rg._adm;
 
-GRANT SELECT ON PARAMETER_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON PARAMETER_CONF TO &rep_schema._adm;
-GRANT SELECT ON PARAMETER_CONF_SEQ TO &rep_schema._sel;
-GRANT SELECT ON PARAMETER_CONF_SEQ TO &rep_schema._adm;
+GRANT SELECT ON PARAMETER_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON PARAMETER_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON PARAMETER_CONF_SEQ TO &rep_schema_rg._sel;
+GRANT SELECT ON PARAMETER_CONF_SEQ TO &rep_schema_rg._adm;
 
-GRANT SELECT ON PARTNAME TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON PARTNAME TO &rep_schema._adm;
+GRANT SELECT ON PARTNAME TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON PARTNAME TO &rep_schema_rg._adm;
 
-GRANT SELECT ON REGISTRATION_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON REGISTRATION_CONF TO &rep_schema._adm;
-GRANT SELECT ON REGISTRATION_CONF_SEQ TO &rep_schema._sel;
-GRANT SELECT ON REGISTRATION_CONF_SEQ TO &rep_schema._adm;
+GRANT SELECT ON REGISTRATION_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON REGISTRATION_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON REGISTRATION_CONF_SEQ TO &rep_schema_rg._sel;
+GRANT SELECT ON REGISTRATION_CONF_SEQ TO &rep_schema_rg._adm;
 
-GRANT SELECT ON RUNMODE_CONF TO &rep_schema._sel;
-GRANT SELECT,UPDATE,DELETE,INSERT ON RUNMODE_CONF TO &rep_schema._adm;
-GRANT SELECT ON RUNMODE_CONF_SEQ TO &rep_schema._sel;
-GRANT SELECT ON RUNMODE_CONF_SEQ TO &rep_schema._adm;
+GRANT SELECT ON RUNMODE_CONF TO &rep_schema_rg._sel;
+GRANT SELECT,UPDATE,DELETE,INSERT ON RUNMODE_CONF TO &rep_schema_rg._adm;
+GRANT SELECT ON RUNMODE_CONF_SEQ TO &rep_schema_rg._sel;
+GRANT SELECT ON RUNMODE_CONF_SEQ TO &rep_schema_rg._adm;
