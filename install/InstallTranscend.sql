beaeecfc195c892a150b9cb0ad81d4b18163c38a
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 19:09:53 +0000

    Ticket #22
    Made changing the default tablespace of the repository schema dependent upon whether the user was created or not.

--- install/InstallTranscend.sql
+++ install/InstallTranscend.sql
@@ -5,41 +5,36 @@ ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
 SPOOL InstallTranscend_&_DATE..log
 
 -- first get the schema for the Transcend repository (tables) first
-ACCEPT td_rep char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
+ACCEPT rep_schema char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
 -- get the tablespace for the repository
 ACCEPT tablespace char default 'TDREP' prompt 'Tablespace in which to install Transcend default repository: [tdrep]: '
 -- get application user
-ACCEPT td_app char default 'TDREP' prompt 'Schema name for the Transcend application [tdrep]: '
+ACCEPT app_schema char default 'TDREP' prompt 'Schema name for the Transcend application [tdrep]: '
 
+-- create the new user or handle the exception that it exists
+@create_user &rep_schema &tablespace
 
--- get the current default tablespace of the repository user
-VARIABLE old_tbspace char(30)
-BEGIN
-   SELECT default_tablespace
-     INTO :old_tbspace
-     FROM dba_users
-    WHERE username=upper('&td_rep');
-END;
-/
-
--- alter the default tablespace of the repository user
-ALTER USER &td_rep DEFAULT TABLESPACE &tablespace;
-ALTER USER &td_rep QUOTA 50M ON &TABLESPACE;
+-- give the user a quota on the tablespace
+ALTER USER &rep_schema QUOTA 50M ON &TABLESPACE;
 
 -- install the repository
-@@transcend_repository &td_rep
+@@transcend_repository &rep_schema
 
 -- create the synonyms between the two if they are different
-@@rep_syns &td_app &td_rep
+@@rep_syns &app_schema &rep_schema
 
 -- install the Transcend application (stored code)
-@@transcend_application &td_app
+@@transcend_application &app_schema
 
 -- set application defaults
-@@td_rep_defaults
+@@rep_schema_defaults
 
 -- set default tablespace back
+-- it was only changed if the user already existed
 BEGIN
-   EXECUTE IMMEDIATE 'alter user &td_rep default tablespace '||:old_tbspace;
+   IF :tbspace_changed = 'yes'
+   THEN
+      EXECUTE IMMEDIATE 'alter user &rep_schema default tablespace '||:old_tbspace;
+   END IF;
 END;
 /
