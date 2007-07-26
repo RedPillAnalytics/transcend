4fe8db3908d289b323b6d649076f13c930699bf0
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 02:24:38 +0000

    Ticket #22.
    Modified to using explicit substitition variable names instead of the default parameter number.

--- install/exec_app_grants.sql
+++ install/exec_app_grants.sql
@@ -3,30 +3,30 @@ PROMPT 'Running exec_app_grants.sql'
 -- grants the role to a repository user
 
 -- &1 IS the application SCHEMA
-
+app_schema = &1
 -- create role for this application
-DROP ROLE &1._app;
-CREATE ROLE &1._app;
+DROP ROLE &app_schema._app;
+CREATE ROLE &app_schema._app;
 
 -- grant execute on components to this role
-GRANT EXECUTE ON STRING_AGG_TYPE to &1._app;
-GRANT EXECUTE ON TD_EXT to &1._app;
-GRANT EXECUTE ON BASETYPE to &1._app;
-GRANT EXECUTE ON APPTYPE to &1._app;
-GRANT EXECUTE ON NOTIFYTYPE to &1._app;
-GRANT EXECUTE ON EMAILTYPE to &1._app;
-GRANT EXECUTE ON TDTYPE to &1._app;
-GRANT EXECUTE ON TD_CORE to &1._app;
-GRANT EXECUTE ON TD_SQL to &1._app;
-GRANT EXECUTE ON FILETYPE to &1._app;
-GRANT EXECUTE ON EXTRACTTYPE to &1._app;
-GRANT EXECUTE ON FEEDTYPE to &1._app;
-GRANT EXECUTE ON TD_DBAPI to &1._app;
-GRANT EXECUTE ON TD_FILEAPI to &1._app;
-GRANT EXECUTE ON TD_CONTROL to &1._app;
-GRANT EXECUTE ON TD_OWBAPI to &1._app;
+GRANT EXECUTE ON STRING_AGG_TYPE to &app_schema._app;
+GRANT EXECUTE ON TD_EXT to &app_schema._app;
+GRANT EXECUTE ON BASETYPE to &app_schema._app;
+GRANT EXECUTE ON APPTYPE to &app_schema._app;
+GRANT EXECUTE ON NOTIFYTYPE to &app_schema._app;
+GRANT EXECUTE ON EMAILTYPE to &app_schema._app;
+GRANT EXECUTE ON TDTYPE to &app_schema._app;
+GRANT EXECUTE ON TD_CORE to &app_schema._app;
+GRANT EXECUTE ON TD_SQL to &app_schema._app;
+GRANT EXECUTE ON FILETYPE to &app_schema._app;
+GRANT EXECUTE ON EXTRACTTYPE to &app_schema._app;
+GRANT EXECUTE ON FEEDTYPE to &app_schema._app;
+GRANT EXECUTE ON TD_DBAPI to &app_schema._app;
+GRANT EXECUTE ON TD_FILEAPI to &app_schema._app;
+GRANT EXECUTE ON TD_CONTROL to &app_schema._app;
+GRANT EXECUTE ON TD_OWBAPI to &app_schema._app;
 
 -- grant select on the object views to this role
-GRANT SELECT ON EMAIL_OT to &1._app;
-GRANT SELECT ON EXTRACT_OT to &1._app;
-GRANT SELECT ON FEED_OT to &1._app;
+GRANT SELECT ON EMAIL_OT to &app_schema._app;
+GRANT SELECT ON EXTRACT_OT to &app_schema._app;
+GRANT SELECT ON FEED_OT to &app_schema._app;
