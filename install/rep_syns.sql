6a3cb6d47a756e9390d99f18e04a7b45ce0b5074
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 20:31:59 +0000

    Ticket #22
    Removing the user of plsql variables in anonymous PLSQL, and instead using the original substitution variables.

--- install/rep_syns.sql
+++ install/rep_syns.sql
@@ -10,36 +10,32 @@ DEFINE rep_schema_rs = &2
 -- first parameter passed is the synonym schema
 -- second parameter passed is the object schema
 
-DECLARE
-   l_syn_schema VARCHAR2(30) := upper('&rep_user_rs');
-   l_obj_schema VARCHAR2(30) := upper('&rep_schema_rs');
 BEGIN   
-   IF l_syn_schema <> l_obj_schema
+   IF upper('&rep_user_rs') <> upper('&rep_schema_rs')
    THEN
       -- tables
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.COUNT_TABLE for '||l_obj_schema||'.COUNT_TABLE';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.DIR_LIST for '||l_obj_schema||'.DIR_LIST';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.ERR_CD for '||l_obj_schema||'.ERR_CD';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_CONF for '||l_obj_schema||'.FILEHUB_CONF';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_DETAIL for '||l_obj_schema||'.FILEHUB_DETAIL';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_OBJ_DETAIL for '||l_obj_schema||'.FILEHUB_OBJ_DETAIL';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.LOGGING_CONF for '||l_obj_schema||'.LOGGING_CONF';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.LOG_TABLE for '||l_obj_schema||'.LOG_TABLE';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.NOTIFY_CONF for '||l_obj_schema||'.NOTIFY_CONF';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.PARTNAME for '||l_obj_schema||'.PARTNAME';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.REGISTRATION_CONF for '||l_obj_schema||'.REGISTRATION_CONF';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.RUNMODE_CONF for '||l_obj_schema||'.RUNMODE_CONF';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.PARAMETER_CONF for '||l_obj_schema||'.PARAMETER_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..COUNT_TABLE for &rep_schema_rs..COUNT_TABLE';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..DIR_LIST for &rep_schema_rs..DIR_LIST';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..ERR_CD for &rep_schema_rs..ERR_CD';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_CONF for &rep_schema_rs..FILEHUB_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_DETAIL for &rep_schema_rs..FILEHUB_DETAIL';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_OBJ_DETAIL for &rep_schema_rs..FILEHUB_OBJ_DETAIL';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..LOGGING_CONF for &rep_schema_rs..LOGGING_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..LOG_TABLE for &rep_schema_rs..LOG_TABLE';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..NOTIFY_CONF for &rep_schema_rs..NOTIFY_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..PARTNAME for &rep_schema_rs..PARTNAME';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..REGISTRATION_CONF for &rep_schema_rs..REGISTRATION_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..RUNMODE_CONF for &rep_schema_rs..RUNMODE_CONF';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..PARAMETER_CONF for &rep_schema_rs..PARAMETER_CONF';
       -- sequences
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_CONF_SEQ for '||l_obj_schema||'.FILEHUB_CONF_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_DETAIL_SEQ for '||l_obj_schema||'.FILEHUB_DETAIL_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.FILEHUB_OBJ_DETAIL_SEQ for '||l_obj_schema||'.FILEHUB_OBJ_DETAIL_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.LOGGING_CONF_SEQ for '||l_obj_schema||'.LOGGING_CONF_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.NOTIFY_CONF_SEQ for '||l_obj_schema||'.NOTIFY_CONF_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.REGISTRATION_CONF_SEQ for '||l_obj_schema||'.REGISTRATION_CONF_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.RUNMODE_CONF_SEQ for '||l_obj_schema||'.RUNMODE_CONF_SEQ';
-      EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.PARAMETER_CONF_SEQ for '||l_obj_schema||'.PARAMETER_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_CONF_SEQ for &rep_schema_rs..FILEHUB_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_DETAIL_SEQ for &rep_schema_rs..FILEHUB_DETAIL_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..FILEHUB_OBJ_DETAIL_SEQ for &rep_schema_rs..FILEHUB_OBJ_DETAIL_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..LOGGING_CONF_SEQ for &rep_schema_rs..LOGGING_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..NOTIFY_CONF_SEQ for &rep_schema_rs..NOTIFY_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..REGISTRATION_CONF_SEQ for &rep_schema_rs..REGISTRATION_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..RUNMODE_CONF_SEQ for &rep_schema_rs..RUNMODE_CONF_SEQ';
+      EXECUTE IMMEDIATE 'create or replace synonym &rep_user_rs..PARAMETER_CONF_SEQ for &rep_schema_rs..PARAMETER_CONF_SEQ';
    END IF;
-      
 END;
 /
