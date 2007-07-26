6a775157137e5d0f7a3bd7c3a16752abcc76b04b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 19:48:26 +0000

    Ticket #22
    Using suffixes to all the variables for the different components.

--- install/rep_syns.sql
+++ install/rep_syns.sql
@@ -1,9 +1,9 @@
 prompt 'Running rep_syns.sql'
 
 -- &1 IS the repository USER
-DEFINE rep_user = &1
+DEFINE rep_user_rs = &1
 -- &2 IS the repository SCHEMA
-DEFINE rep_schema = &2
+DEFINE rep_schema_rs = &2
 
 
 -- create synonyms for repository objects
@@ -11,10 +11,10 @@ DEFINE rep_schema = &2
 -- second parameter passed is the object schema
 
 DECLARE
-   l_syn_schema VARCHAR2(30) := '&rep_user';
-   l_obj_schema VARCHAR2(30) := '&rep_schema';
+   l_syn_schema VARCHAR2(30) := upper('&rep_user_rs');
+   l_obj_schema VARCHAR2(30) := upper('&rep_schema_rs');
 BEGIN   
-   IF upper(l_syn_schema) <> upper(l_obj_schema)
+   IF l_syn_schema <> l_obj_schema
    THEN
       -- tables
       EXECUTE IMMEDIATE 'create synonym '||l_syn_schema||'.COUNT_TABLE for '||l_obj_schema||'.COUNT_TABLE';
