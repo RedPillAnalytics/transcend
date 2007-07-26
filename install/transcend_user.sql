2ccbd531ae9f6dbf6f5ad5b2503ef6bad317ef5e
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 02:17:07 +0000

    Ticket #22.
    Corrected bug.

--- install/transcend_user.sql
+++ install/transcend_user.sql
@@ -1,16 +1,16 @@
 PROMPT 'Running transcend_user.sql'
 -- &1 IS the repository USER
--- &2 IS the repository SCHEMA
--- &3 IS the application SCHEMA
-
 DEFINE rep_user = &1
+-- &2 IS the repository SCHEMA
 DEFINE rep_schema = &2
+-- &3 IS the application SCHEMA
 DEFINE app_schema =&3
+
 -- create synonyms from the user to the repository
-@@rep_syns &rep_user $rep_schema
+@@rep_syns &rep_user &rep_schema
 
 -- create synonyms from the user to the application
-@@app_syns &rep_user &app_schemav
+@@app_syns &rep_user &app_schema
 
 -- need to give roles for the repository and the application
 GRANT &2._adm TO &1;
