93e212aa63c4d2401d9e80dd9f105a2593f7f0d8
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 02:19:14 +0000

    Ticket #22.
    Modified to using explicit substitition variable names instead of the default parameter number.

--- install/app_syns.sql
+++ install/app_syns.sql
@@ -1,10 +1,12 @@
 PROMPT 'Running app_syns.sql'
--- &1  is the synonym schema
--- &2 is the object schema
+-- &1  is the synonym SCHEMA
+DEFINE syn_schema = &1
+-- &2 is the object SCHEMA
+DEFINE obj_schema = &2
 
 DECLARE
-   l_syn_schema VARCHAR2(30) := '&1';
-   l_obj_schema VARCHAR2(30) := '&2';
+   l_syn_schema VARCHAR2(30) := '&syn_schema';
+   l_obj_schema VARCHAR2(30) := '&obj_schema';
 BEGIN   
    IF upper(l_syn_schema) <> upper(l_obj_schema)
    THEN
