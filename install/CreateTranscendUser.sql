f132b020205457e2d3d1d12e3e6c76e7b6b10351
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 21:07:22 +0000

    Ticket #22
    Better comments.

--- install/CreateTranscendUser.sql
+++ install/CreateTranscendUser.sql
@@ -4,15 +4,17 @@ SET echo off
 ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
 SPOOL CreateTranscendUser_&_DATE..log
 
+-- get the user to create
 ACCEPT repo_user char default 'TDREP_USER' prompt 'Username to configure as Transcend repository user [tdrep_user]:'
+-- get the repository to associate with
 ACCEPT repo_schema char default 'TDREP' prompt 'Schema containing Transcend repository for this user [tdrep]:'
+-- get the application to associate with
 ACCEPT app_schema char default 'TDREP' prompt 'Schema containing Transcend application for this user [tdrep]:'
 -- lock the application schema if desired
 ACCEPT response char default 'yes' prompt 'Do you want to lock the &app_schema schema? [yes]:'
 
 -- call the modular script
 @@transcend_user &repo_user &repo_schema &app_schema
-
 
 DECLARE
    l_response VARCHAR2(30) := lower('&response');
