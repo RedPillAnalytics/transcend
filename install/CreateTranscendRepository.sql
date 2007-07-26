21e713f85b7b34926c39ab0ab9137ec207c50883
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 04:06:43 +0000

    Ticket #22
    Added tablespace handling.

--- install/CreateTranscendRepository.sql
+++ install/CreateTranscendRepository.sql
@@ -6,6 +6,23 @@ SPOOL CreateTranscendRepository_&_DATE..log
 
 -- first get the schema for the Transcend repository (tables) first
 ACCEPT td_rep char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
+-- get the tablespace for the repository
+ACCEPT tablespace char default 'TDREP' prompt 'Tablespace in which to install Transcend default repository: [tdrep]: '
+
+-- get the current default tablespace of the repository user
+VARIABLE old_tbspace char(30)
+BEGIN
+   SELECT default_tablespace
+     INTO :old_tbspace
+     FROM dba_users
+    WHERE username=upper('&td_rep');
+END;
+/
+
+-- alter the default tablespace of the repository user
+ALTER USER &td_rep DEFAULT TABLESPACE &tablespace;
+ALTER USER &td_rep QUOTA 50M ON &TABLESPACE;
+
 
 -- install the repository
 @@transcend_repository &td_rep
