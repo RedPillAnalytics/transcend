006244a73149398cce90018603b0e2d60153a686
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 19:22:46 +0000

    Ticket #22
    Moved most of the functionality for creating and modifying users into the transcend_repository.sql script.

--- install/CreateTranscendRepository.sql
+++ install/CreateTranscendRepository.sql
@@ -9,23 +9,8 @@ ACCEPT td_rep char default 'TDREP' prompt 'Schema name for the Transcend reposit
 -- get the tablespace for the repository
 ACCEPT tablespace char default 'TDREP' prompt 'Tablespace in which to install Transcend default repository: [tdrep]: '
 
--- get the current default tablespace of the repository user
-VARIABLE old_tbspace char(30)
-BEGIN
-   SELECT default_tablespace
-     INTO :old_tbspace
-     FROM dba_users
-    WHERE username=upper('&td_rep');
-END;
-/
-
--- alter the default tablespace of the repository user
-ALTER USER &td_rep DEFAULT TABLESPACE &tablespace;
-ALTER USER &td_rep QUOTA 50M ON &TABLESPACE;
-
-
 -- install the repository
-@@transcend_repository &td_rep
+@@transcend_repository &td_rep &tablespace
 
 -- set application defaults
 @@td_rep_defaults
