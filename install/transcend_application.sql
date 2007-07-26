9de20d6f79e769a34bddc07a4ed78388631c5d49
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-26 20:10:07 +0000

    Ticket #22
    Using suffixes to all the variables for the different components.

--- install/transcend_application.sql
+++ install/transcend_application.sql
@@ -2,6 +2,10 @@ PROMPT 'Running transcend_application.sql'
 -- &1 IS the application SCHEMA
 DEFINE app_schema_ta = &1
 
+-- first create the user if it doesn't already exist
+@create_app_user &app_schema_ta
+
+
 -- grant privileges required for package to compile
 -- also privileges needed for the application schema to operate with full power
 @full_app_grants &app_schema_ta
