e9afb2f58431d6398dc7ca178c730c1af3dfc302
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-30 12:11:21 +0000

    Application defaults need to be installed with the repository.

--- trunk/install/td_rep_defaults.sql
+++ trunk/install/td_rep_defaults.sql
@@ -1,9 +1,11 @@
+DEFINE app_schema_tfd = &1
+
 -- set the default logging, registration and runmodes
-EXEC td_control.set_logging_level('default',2,3);
-EXEC td_control.set_runmode;
-EXEC td_control.set_registration;
+EXEC &app_schema_tfd..td_control.set_logging_level('default',2,3);
+EXEC &app_schema_tfd..td_control.set_runmode;
+EXEC &app_schema_tfd..td_control.set_registration;
 
 -- don't register TD_SQL components
 -- that's because DBMS_MONITOR should look for calling modules for tracing
-exec td_control.set_registration('td_sql.exec_sql','noregister');
-exec td_control.set_registration('td_sql.exec_auto','noregister');
+exec &app_schema_tfd..td_control.set_registration('td_sql.exec_sql','noregister');
+exec &app_schema_tfd..td_control.set_registration('td_sql.exec_auto','noregister');
