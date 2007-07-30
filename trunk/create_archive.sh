91efc85b98a1290df47b574dcea4e75d13ee978b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-30 16:27:18 +0000

    Ticket #22
    Removed the version naming from the zipfile.

--- trunk/create_archive.sh
+++ trunk/create_archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r ./transcend_v0_5.zip ddl/*.sql java/* object_views/* install/* plsql/specs/*.*s plsql/wrapped_bodies/*plb plsql/functions/*.fnc
+zip -r ./transcend.zip ddl/*.sql java/* object_views/* install/* plsql/specs/*.*s plsql/wrapped_bodies/*plb plsql/functions/*.fnc
