f6b10d8a19a9cd11fc116c2cf4271516119fc961
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-30 16:55:31 +0000

    Changes merged in from milestone:0.5 release.

--- install/td_rep_defaults.sql
+++ install/td_rep_defaults.sql
@@ -1,3 +1,7 @@
+DEFINE app_schema_tfd = &1
+
+ALTER SESSION SET current_schema=&app_schema_tfd;
+
 -- set the default logging, registration and runmodes
 EXEC td_control.set_logging_level('default',2,3);
 EXEC td_control.set_runmode;
@@ -5,5 +9,7 @@ EXEC td_control.set_registration;
 
 -- don't register TD_SQL components
 -- that's because DBMS_MONITOR should look for calling modules for tracing
-exec td_control.set_registration('td_sql.exec_sql','noregister');
-exec td_control.set_registration('td_sql.exec_auto','noregister');
+exec &app_schema_tfd..td_control.set_registration('td_sql.exec_sql','noregister');
+exec &app_schema_tfd..td_control.set_registration('td_sql.exec_auto','noregister');
+
+ALTER SESSION SET current_schema=&_USER;
