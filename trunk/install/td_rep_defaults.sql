95e03812a0c5cffb19f9df65e17c6b40a2a0cb5e
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-07-30 16:42:46 +0000

    Ticket #22
    Try using CURRENT_SCHEMA as opposed to appending schema name in the front.

--- trunk/install/td_rep_defaults.sql
+++ trunk/install/td_rep_defaults.sql
@@ -1,11 +1,15 @@
 DEFINE app_schema_tfd = &1
 
+ALTER SESSION SET current_schema=&app_schema_tfd;
+
 -- set the default logging, registration and runmodes
-EXEC &app_schema_tfd..td_control.set_logging_level('default',2,3);
-EXEC &app_schema_tfd..td_control.set_runmode;
-EXEC &app_schema_tfd..td_control.set_registration;
+EXEC td_control.set_logging_level('default',2,3);
+EXEC td_control.set_runmode;
+EXEC td_control.set_registration;
 
 -- don't register TD_SQL components
 -- that's because DBMS_MONITOR should look for calling modules for tracing
 exec &app_schema_tfd..td_control.set_registration('td_sql.exec_sql','noregister');
 exec &app_schema_tfd..td_control.set_registration('td_sql.exec_auto','noregister');
+
+ALTER SESSION SET current_schema=&_USER;
