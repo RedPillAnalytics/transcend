f73d2dbbb704adb29a95d1d8a6c7cbd984c856f7
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-08-03 18:31:54 +0000

    Ticket #39
    New table to hold configured repository schemas.

--- ddl/REPOSITORIES_tbl.sql
+++ ddl/REPOSITORIES_tbl.sql
@@ -1,12 +1,8 @@
 DROP TABLE repositories CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE repositories_seq
-/
-
 CREATE TABLE repositories
-       ( repository_id       NUMBER NOT NULL,
-	 repository_schema   VARCHAR2(30) NOT NULL,
+       ( repository_name     VARCHAR2(30) NOT NULL,
 	 created_user	     VARCHAR2(30) DEFAULT sys_context('USERENV','SESSION_USER') NOT NULL,
 	 created_dt	     DATE DEFAULT SYSDATE NOT NULL,
 	 modified_user	     VARCHAR2(30),
@@ -17,9 +13,6 @@ CREATE TABLE repositories
 ALTER TABLE repositories ADD (
   CONSTRAINT repositories_pk
  PRIMARY KEY
- (repository_id)
+ (repository_name)
     USING INDEX)
-/
-
-CREATE SEQUENCE repositories_seq
 /
