ff0e07613effe8a65224b5dd0cecb8184d686fd1
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-08-07 20:13:04 +0000

    Ticket #39
    Modified user feedback wording slightly.

--- install/InstallTranscend.sql
+++ install/InstallTranscend.sql
@@ -5,7 +5,7 @@ ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
 SPOOL InstallTranscend_&_DATE..log
 
 -- first get the schema for the Transcend repository (tables) first
-ACCEPT rep_schema_it char default 'TDREP' prompt 'Schema name for the Transcend repository [tdrep]: '
+ACCEPT rep_schema_it char default 'TDREP' prompt 'Schema name for the Transcend default repository [tdrep]: '
 -- get the tablespace for the repository
 ACCEPT tablespace_it char default 'TDREP' prompt 'Tablespace in which to install Transcend default repository: [tdrep]: '
 -- get application user
