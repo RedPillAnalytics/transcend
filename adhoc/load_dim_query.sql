7217c6465fd40b8777233072ac115b511b08cd0b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-08-14 13:23:26 +0000

    Ticket #37
    Most recent changes.

--- adhoc/load_dim_query.sql
+++ adhoc/load_dim_query.sql
@@ -1,5 +1,7 @@
 SELECT DISTINCT owner,
        table_name,
+       'CASE '||sk||' when -.1 then '||sequence_owner||'.'||sequence_name||' else '||sk||' end '||sk||','
+       ||nk||',' sel1,
        (SELECT stragg('last_value('||column_name||') over (partition by '||nk||' order by '||esd||' ROWS BETWEEN unbounded preceding AND unbounded following)') OVER ( partition BY column_type)
 	  FROM column_conf ic
 	 WHERE ic.owner=owner
@@ -9,29 +11,34 @@ SELECT DISTINCT owner,
        ||sk||','
        ||nk||','
        ||esd||','
-       ||(SELECT stragg(column_name)
-	  FROM column_conf ic
-	 WHERE ic.owner=owner
-	   AND ic.table_name=table_name
-	     AND REGEXP_LIKE(ic.column_type,'scd','i'))
+       ||scd_list
        ||' from '||source_owner||'.'||source_object
        ||' union select '
        ||sk||','
        ||nk||','
        ||esd||','
-       ||(SELECT stragg(column_name)
+       ||scd_list
+       ||' from '||owner||'.'||table_name||')' union_list,
+       'case when '||nk||' <> -.1 then ''Y'' when '||esd||'=LAG(effect_start_dt) over (partition by '||nk||' order by '||esd||','||sk||' desc) then ''N'''
+       ||(SELECT stragg(' WHEN nvl('||column_name||',-.01) < > nvl(LAG('||column_name||') OVER (partition BY '||nk||' ORDER BY '||esd||'),-.01) THEN ''Y''')
 	  FROM column_conf ic
 	 WHERE ic.owner=owner
-	   AND ic.table_name=table_name
-	     AND REGEXP_LIKE(ic.column_type,'scd','i'))
-       ||' from '||owner||'.'||table_name||')' union_list,
-       'case when '||nk||' <> -.1 then ''Y'' when '||esd||'=LAG(effect_start_dt) over (partition by '||nk||' order by '||esd||','||sk||' desc) then ''N'' when ' scd2_list
+	     AND ic.table_name=table_name
+	     AND column_type='scd type 2') 
+       ||' else ''N'' end' scd2_list
   FROM (SELECT owner,
 	       table_name,
 	       column_type,
 	       column_name,
 	       source_object,
 	       source_owner,
+	       sequence_owner,
+	       sequence_name,
+	       (SELECT stragg(column_name)
+		  FROM column_conf ic
+		 WHERE ic.owner=owner
+		   AND ic.table_name=table_name
+		   AND REGEXP_LIKE(ic.column_type,'scd','i')) scd_list,
 	       (SELECT column_name
 		  FROM column_conf ic
 		 WHERE ic.owner=owner
