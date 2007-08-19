478bba06bf2d438a74cd1f2b4a44dd4c24ce5535
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-08-19 03:55:19 +0000

    Ticket #46 Removed RUNMODE attribute.

--- object_views/EMAIL_OT_vw.sql
+++ object_views/EMAIL_OT_vw.sql
@@ -2,8 +2,7 @@ CREATE OR REPLACE VIEW email_ot
 OF emailtype
 WITH object identifier (notify_id)
 as
-SELECT cast('runtime' AS VARCHAR2(10)) runmode,
-       notify_id,
+SELECT notify_id,
        notify_enabled,
        action,
        module,
