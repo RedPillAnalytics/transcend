7b89b58c9488c7f1a525beee043b5011e555d473
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-08-26 22:19:11 +0000

    Ticket #46 Added owner and table_name replacements.

--- adhoc/build_table_query.sql
+++ adhoc/build_table_query.sql
@@ -1,16 +1,16 @@
-var p_table VARCHAR2(30)
-var p_tablespace VARCHAR2(30)
-var p_source_table VARCHAR2(30)
-var p_source_owner VARCHAR2(30)
-var p_seg_attributes VARCHAR2(3)
-var p_owner VARCHAR2(30)
-var p_table VARCHAR2(30)
-var p_partitioning VARCHAR2(3)
-var p_tablespace VARCHAR2(30)
+VAR p_table VARCHAR2(30)
+VAR p_tablespace VARCHAR2(30)
+VAR p_source_table VARCHAR2(30)
+VAR p_source_owner VARCHAR2(30)
+VAR p_seg_attributes VARCHAR2(3)
+VAR p_owner VARCHAR2(30)
+VAR p_table VARCHAR2(30)
+VAR p_partitioning VARCHAR2(3)
+VAR p_tablespace VARCHAR2(30)
 
 EXEC :p_tablespace := NULL;
-EXEC :p_owner := NULL;
-EXEC :p_table := NULL;
+EXEC :p_owner := 'whimport';
+EXEC :p_table := 'ar_transaction_stg';
 EXEC :p_source_owner := 'whdata';
 EXEC :p_source_table := 'ar_transaction_fact';
 EXEC :p_seg_attributes := 'no';
@@ -28,18 +28,40 @@ EXEC DBMS_METADATA.set_transform_param( DBMS_METADATA.session_transform,'SEGMENT
 EXEC DBMS_METADATA.set_transform_param( DBMS_METADATA.session_transform, 'STORAGE',FALSE );
 
 
-SELECT    regexp_replace(REGEXP_REPLACE(DBMS_METADATA.get_ddl( 'TABLE', table_name, owner ),
-                 CASE
-                 -- don't want partitioning
-                 WHEN :p_partitioning = 'no'
-                 -- remove all partitioning
-                 THEN '(\(\s*partition.+\))[[:space:]]*|(partition by).+\)[[:space:]]*'
-                 ELSE NULL
-                 END,
-                 NULL,
+SELECT REGEXP_REPLACE
+          ( REGEXP_REPLACE
+               ( REGEXP_REPLACE
+                    ( REGEXP_REPLACE
+                         ( DBMS_METADATA.get_ddl( 'TABLE', table_name, owner ),
+                           CASE
+                              -- don't want partitioning
+                           WHEN td_ext.get_yn_ind(:p_partitioning) = 'no'
+                                 -- remove all partitioning
+                           THEN '(\(\s*partition.+\))[[:space:]]*|(partition by).+\)[[:space:]]*'
+                              ELSE NULL
+                           END,
+                           NULL,
+                           1,
+                           0,
+                           'in'
+                         ),
+                      '(tablespace)(\s*)([^ ]+)([[:space:]]*)',
+                      '\1\2' || :p_tablespace || '\4',
+                      1,
+                      0,
+                      'i'
+                    ),
+                 '(\."?)(' || :p_source_table || ')(\w*)("?)',
+                 '.' || :p_table || '\3',
                  1,
-					 0,'in'),'(tablespace)(\s*)([^ ]+)([[:space:]]*)','\1\2'||:p_tablespace||'\4',1,0,'i') table_ddl
-  from all_tables
- where owner=upper(:p_source_owner)
-   AND table_name=upper(:p_source_table)
-
+                 0,
+                 'i'
+               ),
+            '(")?(' || :p_source_owner || ')("?\.)',
+            :p_owner || '.',
+            1,
+            0,
+            'i'
+          ) table_ddl
+  FROM all_tables
+ WHERE owner = UPPER( :p_source_owner ) AND table_name = UPPER( :p_source_table )
