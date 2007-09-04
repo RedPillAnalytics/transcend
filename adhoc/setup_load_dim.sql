603e65b4e8650df6ae19bb9b3634575e00bf0b69
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-09-04 21:56:13 +0000

    Ticket #37 Added truncate statements.

--- adhoc/setup_load_dim.sql
+++ adhoc/setup_load_dim.sql
@@ -1,10 +1,16 @@
-insert into dimension_conf(owner,table_name,source_owner,source_object,replace_method,sequence_owner,sequence_name) values ('stewart','test_dim','stewart','test_stg','exchange','stewart','test_dim_seq');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','test_key','surrogate key');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','birthdate','scd type 1');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','name','scd type 2');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','zip','scd type 2');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','zip_plus4','scd type 2');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','nat_key','natural key');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','effect_start_dt','effective start date');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','effect_end_dt','effective end date');
-insert into column_conf(owner,table_name,column_name,column_type) values ('stewart','test_dim','current_ind','current indicator');
+TRUNCATE TABLE dimension_conf;
+TRUNCATE TABLE column_conf;
+TRUNCATE TABLE index_maint_conf;
+TRUNCATE TABLE constraint_maint_conf;
+
+INSERT INTO dimension_conf(owner,table_name,source_owner,source_object,replace_method,sequence_owner,sequence_name) 
+       VALUES ('stewart','test_dim','stewart','test_stg','rename','stewart','test_dim_seq');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','test_key','surrogate key');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','birthdate','scd type 1');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','name','scd type 2');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','zip','scd type 2');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','zip_plus4','scd type 2');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','nat_key','natural key');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','effect_start_dt','effective start date');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','effect_end_dt','effective end date');
+INSERT INTO column_conf(owner,table_name,column_name,column_type) VALUES ('stewart','test_dim','current_ind','current indicator');
