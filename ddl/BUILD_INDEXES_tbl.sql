ce7a264fd149abdcf5a81caafb146993db7f1d30
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-09-28 21:00:30 +0000

    Renaming to TD_BUILD_IDX_GTT.

--- ddl/BUILD_INDEXES_tbl.sql
+++ ddl/BUILD_INDEXES_tbl.sql
@@ -1,17 +1,16 @@
-DROP TABLE build_indexes purge
+DROP TABLE td_build_idx_gtt purge
 /
 
-CREATE global TEMPORARY TABLE build_indexes
-( index_owner VARCHAR2(30),
-  index_name VARCHAR2(30),
-  source_owner VARCHAR2(30),
-  source_index VARCHAR2(30),
-  partitioned VARCHAR2(3),
-  uniqueness VARCHAR2(10),
-  index_type VARCHAR2(20),
-  index_ddl VARCHAR2(4000),
-  rename_ddl VARCHAR2(4000),
-  rename_msg VARCHAR2(4000)
+CREATE global TEMPORARY TABLE td_build_idx_gtt
+( 
+  index_owner 		      VARCHAR2(30),
+  index_name 		      VARCHAR2(30),
+  src_index_owner 	      VARCHAR2(30),
+  src_index_name 	      VARCHAR2(30),
+  create_ddl 		      VARCHAR2(4000),
+  create_msg 		      VARCHAR2(4000),
+  rename_ddl 		      VARCHAR2(4000),
+  rename_msg 		      VARCHAR2(4000)
 )
 ON COMMIT DELETE ROWS
 /
