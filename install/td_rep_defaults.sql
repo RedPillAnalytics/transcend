4359aa85c64a9194dd51a591a4621cef9414c73d
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-10-18 18:44:18 +0000

    Corrected bug with registration not getting set.
    Since the implementation of TD_INST, I no longer have to put 'noregister' values for the TD_SQL components.

--- install/td_rep_defaults.sql
+++ install/td_rep_defaults.sql
@@ -7,9 +7,4 @@ EXEC td_control.set_logging_level('default',2,3);
 EXEC td_control.set_runmode;
 EXEC td_control.set_registration;
 
--- don't register TD_SQL components
--- that's because DBMS_MONITOR should look for calling modules for tracing
-exec &app_schema_tfd..td_control.set_registration('td_sql.exec_sql','noregister');
-exec &app_schema_tfd..td_control.set_registration('td_sql.exec_auto','noregister');
-
 ALTER SESSION SET current_schema=&_USER;
