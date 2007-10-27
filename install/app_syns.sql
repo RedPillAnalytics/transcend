0293ce9c38d62ea3837f67c880f3007bd2ced012
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-10-27 02:40:18 +0000

    ticket:50 TD_DBAPI renamed to TD_DDL and TD_FILEAPI renamed to TD_FILES.

--- install/app_syns.sql
+++ install/app_syns.sql
@@ -8,8 +8,10 @@ BEGIN
    IF upper('&syn_schema_as') <> upper('&obj_schema_as')
    THEN
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_ext for &obj_schema_as..td_ext';
-      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_fileapi for &obj_schema_as..td_fileapi';
-      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_dbapi for &obj_schema_as..td_dbapi';
+      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_files for &obj_schema_as..td_files';
+      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_host for &obj_schema_as..td_host';
+      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_ddl for &obj_schema_as..td_ddl';
+      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_etl for &obj_schema_as..td_etl';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_owbapi for &obj_schema_as..td_owbapi';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_control for &obj_schema_as..td_control';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_inst for &obj_schema_as..td_inst';
