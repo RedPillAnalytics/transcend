acab7a2318715254c82f56739139d797df3cd43e
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-10-27 23:47:35 +0000

    ticket:50 TD_OWBAPI renamed to TD_OWB.

--- install/app_syns.sql
+++ install/app_syns.sql
@@ -12,7 +12,7 @@ BEGIN
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_host for &obj_schema_as..td_host';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_ddl for &obj_schema_as..td_ddl';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_etl for &obj_schema_as..td_etl';
-      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_owbapi for &obj_schema_as..td_owbapi';
+      EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_owb for &obj_schema_as..td_owb';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_control for &obj_schema_as..td_control';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..td_inst for &obj_schema_as..td_inst';
       EXECUTE IMMEDIATE 'create or replace synonym &syn_schema_as..stragg for &obj_schema_as..stragg';
