ce08a88dac4f3505e1560e5ec073f1431e5ddd35
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-12 20:13:52 +0000

    ticket:47 Renamed come of the column names and parameter names to not include NOTIFICATION when that should be obvious.

--- object_views/NOTIFICATION_OV_vw.sql
+++ object_views/NOTIFICATION_OV_vw.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE VIEW notification_ov
 OF notification_ot
-WITH object identifier (notification_label, module, action)
+WITH object identifier ( label, module, action )
 as
-SELECT notification_label,
+SELECT label,
        module,
        action,
-       notification_method,
-       notification_enabled,
-       notification_required,
+       method,
+       enabled,
+       required,
        subject,
        message,
        sender,
        recipients
   FROM notification_conf
   JOIN notification_events
-       USING (module,action)
+       USING ( module,action )
 /
