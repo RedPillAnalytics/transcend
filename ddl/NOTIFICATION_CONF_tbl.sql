1713b89366bd8114ed94447091b252808f42e28d
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-12 19:41:03 +0000

    ticket:47 SENDER column belongs in NOTIFICATION_CONF, not in NOTIFICATION_EVENTS.

--- ddl/NOTIFICATION_CONF_tbl.sql
+++ ddl/NOTIFICATION_CONF_tbl.sql
@@ -2,17 +2,18 @@ DROP TABLE notification_conf CASCADE CONSTRAINTS purge
 /
 
 CREATE TABLE notification_conf
-       ( notification_label       VARCHAR2(40) NOT NULL,
-	 module        		  VARCHAR2(48) NOT NULL,
-	 action        		  VARCHAR2(32) NOT NULL,
-	 notification_method      VARCHAR2(20) NOT NULL,
-	 notification_enabled     VARCHAR2(3) DEFAULT 'yes',
-	 notification_required	  VARCHAR2(3) DEFAULT 'no',
-	 recipients          	  VARCHAR2(2000) NOT NULL,
-	 created_user     	  VARCHAR2(30) DEFAULT SYS_CONTEXT('USERENV','SESSION_USER') NOT NULL,
-	 created_dt   		  DATE DEFAULT SYSDATE NOT NULL,
-	 modified_user       	  VARCHAR2(30),
-	 modified_dt         	  DATE
+       ( label		   VARCHAR2(40) NOT NULL,
+	 module        	   VARCHAR2(48) NOT NULL,
+	 action        	   VARCHAR2(32) NOT NULL,
+	 method      	   VARCHAR2(20) NOT NULL,
+	 enabled     	   VARCHAR2(3) DEFAULT 'yes',
+	 required	   VARCHAR2(3) DEFAULT 'no',
+	 sender            VARCHAR2(1024),
+	 recipients        VARCHAR2(2000) NOT NULL,
+	 created_user      VARCHAR2(30) DEFAULT SYS_CONTEXT('USERENV','SESSION_USER') NOT NULL,
+	 created_dt   	   DATE DEFAULT SYSDATE NOT NULL,
+	 modified_user     VARCHAR2(30),
+	 modified_dt       DATE
        )
 /
 
