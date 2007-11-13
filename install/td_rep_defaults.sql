ff6944d613b5c797027453a452998f17337f802b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-13 20:47:47 +0000

    ticket:47 Added calls to ADD_NOTIFICATION_EVENT to set up size threshhold parameters for TD_FILES.

--- install/td_rep_defaults.sql
+++ install/td_rep_defaults.sql
@@ -7,4 +7,8 @@ EXEC td_control.set_logging_level('default',2,3);
 EXEC td_control.set_runmode;
 EXEC td_control.set_registration;
 
+-- add notification events
+EXEC td_control.add_notification_event('file_ot.audit_file','file too large','File outside size threshholds','The file referenced below is larger than the configured threshhold:');
+EXEC td_control.add_notification_event('file_ot.audit_file','file too small','File outside size threshholds','The file referenced below is smaller than the configured threshhold:');
+
 ALTER SESSION SET current_schema=&_USER;
