2099e7c3fb7fab68a540e0224d1baa67c28a852b
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-14 22:07:01 +0000

    Removed sequence generated primary key from configuration table.

--- ddl/LOGGING_CONF_tbl.sql
+++ ddl/LOGGING_CONF_tbl.sql
@@ -1,12 +1,8 @@
 DROP TABLE logging_conf CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE logging_conf_seq
-/
-
 CREATE TABLE logging_conf
-       ( logging_id	  NUMBER NOT NULL,
-	 logging_level    NUMBER not NULL,
+       ( logging_level    NUMBER not NULL,
 	 debug_level 	  NUMBER NOT NULL,
 	 module 	  VARCHAR2(48) NOT NULL,
 	 created_user     VARCHAR2(30) DEFAULT sys_context('USERENV','SESSION_USER') NOT NULL,
@@ -19,9 +15,6 @@ CREATE TABLE logging_conf
 ALTER TABLE logging_conf ADD (
   CONSTRAINT logging_conf_pk
  PRIMARY KEY
- (logging_id)
+ (module)
     USING INDEX)
-/
-
-CREATE SEQUENCE logging_conf_seq
 /
