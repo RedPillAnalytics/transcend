07ac43e3245fc3c4ad9eb9495e98b218f66d3dee
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-15 19:13:40 +0000

    Removed sequence-generated primary key from PARAMETER_CONF.

--- ddl/PARAMETER_CONF_tbl.sql
+++ ddl/PARAMETER_CONF_tbl.sql
@@ -1,12 +1,8 @@
 DROP TABLE parameter_conf CASCADE CONSTRAINTS purge
 /
 
-DROP SEQUENCE parameter_conf_seq
-/
-
 CREATE TABLE parameter_conf
-       ( parameter_id	NUMBER NOT NULL,
-	 name		VARCHAR2(40) NOT NULL,
+       ( name		VARCHAR2(40) NOT NULL,
 	 value 		VARCHAR2(40),
 	 module 	VARCHAR2(48) NOT NULL,
 	 created_user   VARCHAR2(30) DEFAULT sys_context('USERENV','SESSION_USER') NOT NULL,
@@ -21,12 +17,9 @@ COMMENT ON TABLE parameter_conf IS 'table to hold session level parameters';
 ALTER TABLE parameter_conf ADD (
   CONSTRAINT parameter_conf_pk
  PRIMARY KEY
- (parameter_id)
+ (name,module)
     USING INDEX)
 /
 
 -- make sure that 'default' is not added here
 ALTER TABLE parameter_conf ADD CONSTRAINT parameter_conf_ck1 CHECK (lower(value) <> 'default');
-
-CREATE SEQUENCE parameter_conf_seq
-/
