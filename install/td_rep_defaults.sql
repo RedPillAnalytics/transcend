3e77179465d8b11bb6542b9c574626d95510b3ba
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-19 18:51:01 +0000

    Replaced calls to ADD_NOTIFICATION_EVENT with SET_NOTIFICATION_EVENT.

--- install/td_rep_defaults.sql
+++ install/td_rep_defaults.sql
@@ -8,7 +8,7 @@ EXEC td_control.set_runmode;
 EXEC td_control.set_registration;
 
 -- add notification events
-EXEC td_control.add_notification_event('file_ot.audit_file','file too large','File outside size threshholds','The file referenced below is larger than the configured threshhold:');
-EXEC td_control.add_notification_event('file_ot.audit_file','file too small','File outside size threshholds','The file referenced below is smaller than the configured threshhold:');
+EXEC td_control.set_notification_event('file_ot.audit_file','file too large','File outside size threshholds','The file referenced below is larger than the configured threshhold:');
+EXEC td_control.set_notification_event('file_ot.audit_file','file too small','File outside size threshholds','The file referenced below is smaller than the configured threshhold:');
 
 ALTER SESSION SET current_schema=&_USER;
