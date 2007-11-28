0c30f78209c9df7c44415d56ac9e5e04a4281b64
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-28 22:24:36 +0000

    ticket:50 Rewrote to be an Evolve-only installation script.

--- evolve/install/install.sql
+++ evolve/install/install.sql
@@ -5,21 +5,22 @@ SET timing off
 ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
 SPOOL InstallTranscend_&_DATE..log
 
--- first get the schema for the Transcend repository (tables) first
-ACCEPT rep_schema char default 'TDSYS' prompt 'Schema name for the Transcend default repository [tdsys]: '
+-- get the CURRENT_SCHEMA
+VARIABLE current_schema char(30)
+EXEC :current_schema := sys_context('USERENV','CURRENT_SCHEMA');
+
+-- get the schema for the Evolve repository (tables)
+ACCEPT rep_schema char default 'TDSYS' prompt 'Schema name for the Evolve default repository [tdsys]: '
 -- get the tablespace for the repository
 ACCEPT tablespace char default 'TDSYS' prompt 'Tablespace in which to install Transcend default repository: [tdsys]: '
--- get application user
+-- get the schema for the Evolve application (PL/SQL and Java code)
 ACCEPT app_schema char default 'TDSYS' prompt 'Schema name for the Transcend application [tdsys]: '
 
 WHENEVER sqlerror exit sql.sqlcode
 
-VARIABLE old_tbspace char(30)
 DECLARE
    e_user_exists EXCEPTION;
    PRAGMA EXCEPTION_INIT( e_user_exists, -1920 );
-   e_no_tbspace	 EXCEPTION;
-   PRAGMA EXCEPTION_INIT( e_no_tbspace, -959 );
 BEGIN
    BEGIN
       EXECUTE IMMEDIATE 'CREATE USER tdsys identified by no2tdsys';
@@ -40,10 +41,14 @@ ALTER SESSION SET current_schema=tdsys;
 @../plsql/specs/TD_INSTALL.pks
 @../plsql/wrapped_bodies/TD_INSTALL.plb
 
-SET termout off
+BEGIN
+   ALTER SESSION SET current_schema=:current_schema;
+   -- build the system repository
+   tdsys.td_install.build_sys_repo( p_schema=> 'tdsys', p_tablespace => '&tablespace' );
 
--- build the system repository
-EXEC tdsys.td_install.build_sys_repo( p_tablespace => '&tablespace' );
+   -- create the Evolve repository
+   tdsys.td_install.build_evolve_repo( p_schema => '&rep_user', p_tablespace => '&tablespace');
 
--- create the Evolve repository
-EXEC tdsys.td_install.build_repo( p_owner => '&rep_user', p_tablespace => '&tablespace');
+   -- create the Evolve application
+   tdsys.td_install.build_evolve_repo( p_schema => '&app_user', p_repository => '&rep_schema');
+END;
