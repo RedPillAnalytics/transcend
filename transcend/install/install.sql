7de0a5f50880426d864eeced53ed1d0aca9002e8
Stewart Bryson <stewart.bryson@rittmanmead.com>
2007-11-29 20:48:59 +0000

    ticket:50 Moving Transcend pieces to the new Transcend directory structure.

---
+++ transcend/install/install.sql
@@ -0,0 +1,115 @@
+PROMPT 'Running install.sql'
+SET serveroutput on size unlimited
+SET timing off
+ALTER SESSION SET nls_date_format = 'yyyymmdd_hhmiss';
+SPOOL InstallTranscend_&_DATE..log
+
+-- get the schema for the Transcend repository (tables)
+ACCEPT rep_schema char default 'TDSYS' prompt 'Schema name for the Transcend default repository [tdsys]: '
+-- get the tablespace for the repository
+ACCEPT tablespace char default 'TDSYS' prompt 'Tablespace in which to install Transcend default repository: [tdsys]: '
+-- get the schema for the Transcend application (PL/SQL and Java code)
+ACCEPT app_schema char default 'TDSYS' prompt 'Schema name for the Transcend application [tdsys]: '
+
+WHENEVER sqlerror exit sql.sqlcode
+
+DECLARE
+   e_user_exists EXCEPTION;
+   PRAGMA EXCEPTION_INIT( e_user_exists, -1920 );
+BEGIN
+   BEGIN
+      EXECUTE IMMEDIATE 'CREATE USER tdsys identified by no2tdsys';
+   EXCEPTION
+      WHEN e_user_exists
+      THEN
+        NULL;
+   END;
+END;
+/
+
+-- needed to interact with users and their tablespaces
+GRANT SELECT ANY dictionary TO tdsys;
+
+ALTER SESSION SET current_schema=tdsys;
+
+-- install the installation package
+@../../plsql/specs/TD_INSTALL.pks
+@../../plsql/wrapped_bodies/TD_INSTALL.plb
+
+BEGIN
+   EXECUTE IMMEDIATE 'ALTER SESSION SET current_schema='||:current_schema;
+END;
+/
+
+-- build the system repository
+EXEC tdsys.td_install.build_sys_repo( p_schema=> 'tdsys', p_tablespace => '&tablespace' );
+
+-- create the Transcend repository
+EXEC tdsys.td_install.build_transcend_repo( p_schema => '&rep_schema', p_tablespace => '&tablespace');
+
+-- create the Trancend application
+EXEC tdsys.td_install.build_transcend_app( p_schema => '&app_schema', p_repository => '&rep_schema');
+
+-- now install the Evolve code
+-- first drop the types
+EXEC tdsys.td_install.drop_trascend_types;
+
+
+-- Install the Evolve pieces
+
+--CREATE java stored procedure
+@../java/TdCore.jvs
+
+--CREATE core pieces
+@../plsql/specs/STRING_AGG_OT.tps
+@../plsql/wrapped_bodies/STRING_AGG_OT.plb
+@../plsql/wrapped_bodies/STRAGG.plb
+@../plsql/specs/TD_EXT.pks
+@../plsql/wrapped_bodies/TD_EXT.plb
+@../plsql/specs/TD_INST.pks
+@../plsql/wrapped_bodies/TD_INST.plb
+
+--CREATE targeted types, packages and object views
+@../plsql/specs/APP_OT.tps
+@../plsql/wrapped_bodies/APP_OT.plb
+@../plsql/specs/NOTIFICATION_OT.tps
+@../plsql/wrapped_bodies/NOTIFICATION_OT.plb
+@../object_views/NOTIFICATION_OV_vw.sql
+@../plsql/specs/EVOLVE_OT.tps
+@../plsql/wrapped_bodies/EVOLVE_OT.plb
+@../plsql/specs/TD_SQL.pks
+@../plsql/wrapped_bodies/TD_SQL.plb
+@../plsql/specs/TD_HOST.pks
+@../plsql/wrapped_bodies/TD_HOST.plb
+
+--CREATE callable packages
+@../plsql/specs/TD_EVOLVE_ADM.pks
+@../plsql/wrapped_bodies/TD_EVOLVE_ADM.plb
+
+
+-- Install the Transcend Pieces
+
+--CREATE targeted _ots, packages and object views
+@../plsql/specs/TD_DDL.pks
+@../plsql/wrapped_bodies/TD_DDL.plb
+@../plsql/specs/FILE_OT.tps
+@../plsql/wrapped_bodies/FILE_OT.plb
+@../plsql/specs/EXTRACT_OT.tps
+@../plsql/wrapped_bodies/EXTRACT_OT.plb
+@../object_views/EXTRACT_OV_vw.sql
+@../plsql/specs/FEED_OT.tps
+@../plsql/wrapped_bodies/FEED_OT.plb
+@../object_views/FEED_OV_vw.sql
+@../plsql/specs/DIMENSION_OT.tps
+@../plsql/wrapped_bodies/DIMENSION_OT.plb
+@../object_views/DIMENSION_OV_vw.sql
+
+--CREATE callable packages
+@../plsql/specs/TD_ETL.pks
+@../plsql/wrapped_bodies/TD_ETL.plb
+@../plsql/specs/TD_FILES.pks
+@../plsql/wrapped_bodies/TD_FILES.plb
+@../plsql/specs/TD_CONTROL.pks
+@../plsql/wrapped_bodies/TD_CONTROL.plb
+@../plsql/specs/TD_OWB.pks
+@../plsql/wrapped_bodies/TD_OWB.plb
