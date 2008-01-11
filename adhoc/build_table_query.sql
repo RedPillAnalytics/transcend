04e6591118025b62ad9bc35418274152779a51c7
Stewart Bryson <stewart.bryson@rittmanmead.com>
2008-01-11 19:58:35 +0000

    The existence of "where constraint_type='P'" after the join was causing the left join to be invalid. Added the CONSTRAINT_TYPE filter to the join criteria.

--- adhoc/build_table_query.sql
+++ adhoc/build_table_query.sql
@@ -14,12 +14,12 @@ VAR p_partitioning VARCHAR2(3)
 VAR p_tablespace VARCHAR2(30)
 
 EXEC :p_tablespace := NULL;
-EXEC :p_owner := 'whstage';
-EXEC :p_table := 'td$customer_dim';
-EXEC :p_source_owner := 'whdata';
-EXEC :p_source_table := 'customer_dim';
+EXEC :p_owner := 'stewart';
+EXEC :p_table := 'td$test_dim';
+EXEC :p_source_owner := 'stewart';
+EXEC :p_source_table := 'test_dim';
 EXEC :p_seg_attributes := 'no';
-EXEC :p_partitioning := 'yes';
+EXEC :p_partitioning := 'no';
 EXEC :p_tablespace := 'default';
 
 -- don't want any constraints pulled
@@ -52,14 +52,14 @@ SELECT REGEXP_REPLACE(
                                                        0,
                                                        'i'
                                                      ),
-                                       '(\.)("?)(' || p_source_table || ')("?)',
-                                       '\1' || UPPER( p_table ),
+                                       '(\.)("?)(' || :p_source_table || ')("?)',
+                                       '\1' || UPPER( :p_table ),
                                        1,
                                        0,
                                        'i'
                                      ),
-                       '(table)(\s+)("?)(' || p_source_owner || ')("?)(\.)',
-                       '\1\2' || UPPER( p_owner ) || '\6',
+                       '(table)(\s+)("?)(' || :p_source_owner || ')("?)(\.)',
+                       '\1\2' || UPPER( :p_owner ) || '\6',
                        1,
                        0,
                        'i'
@@ -69,7 +69,7 @@ SELECT REGEXP_REPLACE(
           -- IN that procedure, after cloning the indexes, the table is renamed
           -- we have to rename the indexes back to their original names
           ' alter table '
-       || UPPER( p_source_owner || '.' || p_source_table )
+       || UPPER( :p_source_owner || '.' || :p_source_table )
        || ' rename constraint '
        || CASE generic_con
              WHEN 'Y'
@@ -89,21 +89,21 @@ SELECT REGEXP_REPLACE(
              ELSE con_rename
           END
        || ' on table '
-       || UPPER( p_source_owner || '.' || p_source_table )
+       || UPPER( :p_source_owner || '.' || :p_source_table )
        || ' renamed to '
        || source_constraint rename_msg,
        iot_type
-  INTO l_table_ddl,
-       l_rename_ddl,
-       l_rename_msg,
-       l_iot_type
+  INTO :l_table_ddl,
+       :l_rename_ddl,
+       :l_rename_msg,
+       :l_iot_type
   FROM ( SELECT
-                -- this regular expression evaluates p_TABLESPACE and modifies the DDL accordingly
+                -- this regular expression evaluates :p_TABLESPACE and modifies the DDL accordingly
                 REGEXP_REPLACE
                           (
-                            -- this regular expression evaluates p_PARTITIONING paramater and removes partitioning information if necessary
+                            -- this regular expression evaluates :p_PARTITIONING paramater and removes partitioning information if necessary
                             REGEXP_REPLACE( table_ddl,
-                                            CASE td_core.get_yn_ind( p_partitioning )
+                                            CASE td_core.get_yn_ind( :p_partitioning )
                                                -- don't want partitioning
                                             WHEN 'no'
                                                   -- remove all partitioning
@@ -117,11 +117,11 @@ SELECT REGEXP_REPLACE(
                                           ),
                             '(tablespace)(\s*)([^ ]+)([[:space:]]*)',
                             CASE
-                               WHEN p_tablespace IS NULL
+                               WHEN :p_tablespace IS NULL
                                   THEN '\1\2\3\4'
-                               WHEN p_tablespace = 'default'
+                               WHEN :p_tablespace = 'default'
                                   THEN NULL
-                               ELSE '\1\2' || UPPER( p_tablespace ) || '\4'
+                               ELSE '\1\2' || UPPER( :p_tablespace ) || '\4'
                             END,
                             1,
                             0,
@@ -141,7 +141,7 @@ SELECT REGEXP_REPLACE(
           FROM ( SELECT
                         -- IF con_rename already exists (constructed below), then we will try to rename the constraint to something generic
                              -- this name will only be used when con_rename name already exists
-                        UPPER(    SUBSTR( p_table, 1, 24 )
+                        UPPER(    SUBSTR( :p_table, 1, 24 )
                                || '_'
                                || con_ext
                                || CASE constraint_type
@@ -154,15 +154,15 @@ SELECT REGEXP_REPLACE(
                              ) con_rename_adj,
                         iot_type, con_rename, table_ddl, constraint_name_confirm, source_owner, source_constraint,
                         index_owner, index_name
-                  FROM ( SELECT DBMS_METADATA.get_ddl( 'TABLE', table_name, owner ) table_ddl, iot_type,
-                                owner source_owner, constraint_name source_constraint, constraint_type, index_owner,
-                                index_name,
+                  FROM ( SELECT DBMS_METADATA.get_ddl( 'TABLE', at.table_name, at.owner ) table_ddl, iot_type,
+                                at.owner source_owner, constraint_name source_constraint, constraint_type, index_owner,
+                                index_name, CASE WHEN constraint_name IS NULL THEN 'N' ELSE 'Y' END key_exists,
                                 
                                 -- this is the constraint name that will be used if it doesn't already exist
                                           -- basically, all cases of the previous table name are replaced with the new table name
                                 UPPER( REGEXP_REPLACE( constraint_name,
-                                                       '(")?' || p_source_table || '(")?',
-                                                       p_table,
+                                                       '(")?' || :p_source_table || '(")?',
+                                                       :p_table,
                                                        1,
                                                        0,
                                                        'i'
@@ -174,17 +174,19 @@ SELECT REGEXP_REPLACE(
                                       THEN 'F'
                                    ELSE constraint_type || 'K'
                                 END con_ext
-                          FROM all_tables
+                           FROM all_tables at
                                          -- joining here to get the primary key for the table (if it exists)
                                          -- this is used to handle IOT's correctly
-                               LEFT JOIN all_constraints USING( owner, table_name )
-                         WHERE owner = UPPER( p_source_owner )
-                           AND table_name = UPPER( p_source_table )
-                           AND constraint_type = 'P' ) g1
+                           LEFT JOIN all_constraints ac
+				ON at.table_name = ac.table_name AND
+				at.owner = ac.owner AND
+				ac.constraint_type='P'
+                         WHERE at.owner = UPPER( :p_source_owner )
+                           AND at.table_name = UPPER( :p_source_table )) g1
                        LEFT JOIN
                        
                        -- joining here to see if the proposed constraint_name (con_rename) actually exists
                        ( SELECT owner constraint_owner_confirm, constraint_name constraint_name_confirm
                           FROM all_constraints ) g2
-                       ON g1.con_rename = g2.constraint_name_confirm AND g2.constraint_owner_confirm = UPPER( p_owner )
+                       ON g1.con_rename = g2.constraint_name_confirm AND g2.constraint_owner_confirm = UPPER( :p_owner )
                        ))
