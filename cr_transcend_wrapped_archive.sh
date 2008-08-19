88ca9c856cbebce68f457215ff641a1a3410fe47
Stewart Bryson <stewart.bryson@rittmanmead.com>
2008-08-19 19:40:12 +0000

    Added .sql to the end of the installation script wildcards used in the create_archive shell scripts.

--- cr_transcend_wrapped_archive.sh
+++ cr_transcend_wrapped_archive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r ./transcend_wrapped.zip ./plsql/specs/*.*s ./plsql/wrapped_bodies/*plb  ./install/*evolve* ./install/*tdsys* ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/wrapped_bodies/*plb
-zip -r ./transcend_wrapped.zip ./install/*transcend* ./transcend/plsql/specs/*.*s ./transcend/plsql/wrapped_bodies/*plb
+zip -r ./transcend_wrapped.zip ./plsql/specs/*.*s ./plsql/wrapped_bodies/*plb  ./install/*evolve*.sql ./install/*tdsys*.sql ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/wrapped_bodies/*plb
+zip -r ./transcend_wrapped.zip ./install/*transcend*.sql ./transcend/plsql/specs/*.*s ./transcend/plsql/wrapped_bodies/*plb
