af2aa6569cc839074d68502fef791bb7a53d8e25
Stewart Bryson <stewart.bryson@rittmanmead.com>
2009-06-04 19:13:39 +0000

    see #139. Provide the functionality for UNUSABLE_INDEXES to operate with subpartitions.
    POPULATE_PARTNAME now works, and the join to TD_PART_GTT in UNUSABLE_INDEXES is now functioning as well.

--- adhoc/unusable_indexes_query.sql
+++ adhoc/unusable_indexes_query.sql
@@ -10,14 +10,16 @@ var p_index_regexp VARCHAR2(30)
 var p_part_type VARCHAR2(30)
 var p_partname VARCHAR2(30)
 var p_source_object VARCHAR2(30)
+var l_part_type VARCHAR2(10)
 
-EXEC :p_owner := 'whdata';
-EXEC :p_table := 'ar_transaction_fact';
+EXEC :p_owner := 'eusb009';
+EXEC :p_table := 'insurquote_policy';
 EXEC :p_part_type := NULL;
 EXEC :p_index_type := NULL;
-EXEC :p_index_regexp := '_r$';
+EXEC :p_index_regexp := NULL;
 EXEC :p_partname := NULL;
-EXEC :p_source_object := 'ar_transaction_fact_stg';
+EXEC :p_source_object := 'insurquote_policy_stg';
+EXEC :l_part_type := 'subpart';
 
 SET feedback on
 SET echo on
@@ -33,72 +35,66 @@ SELECT *
                 THEN NULL
                 ELSE ' modify partition ' || partition_name
                 END
-                || ' unusable' ddl,
-                idx_ddl_type, partition_name, partition_position,
+                || ' unusable' DDL,
+                idx_ddl_type, partition_name,
                 SUM( CASE idx_ddl_type
                      WHEN 'I'
                      THEN 1
                      ELSE 0
-                     END ) OVER( partition BY 1 ) num_indexes,
+                     END ) OVER( PARTITION BY 1 ) num_indexes,
                 SUM( CASE idx_ddl_type
                      WHEN 'P'
                      THEN 1
                      ELSE 0
-                     END
-                   ) OVER( partition BY 1 ) num_partitions,
+                     END ) OVER( PARTITION BY 1 ) num_partitions,
                 CASE idx_ddl_type
                 WHEN 'I'
                 THEN ai_status
                 ELSE aip_status
-                END status,
-		partitioned,
-		include
-           FROM ( SELECT index_type, owner, ai.index_name,
-                         partition_name, aip.partition_position,
+                END status, include, partition_position
+           FROM ( SELECT partition_position, index_type, owner, ai.index_name, partition_name,
                          partitioned, aip.status aip_status,
                          ai.status ai_status,
                          CASE
-                         WHEN partition_name IS NULL
-                      OR partitioned = 'NO'
+                         WHEN partition_name IS NULL OR partitioned = 'NO'
                          THEN 'I'
                          ELSE 'P'
                          END idx_ddl_type,
-			 CASE
-			 WHEN ( :p_source_object IS NOT NULL
-				OR :p_partname IS NOT NULL)
-		     AND ( partitioned = 'YES')
-		     AND partition_name IS null
-			 THEN 'N'
-			 ELSE 'Y'
-			 END include		 
-		    FROM partname JOIN all_ind_partitions aip
-                         USING( partition_name )
-                   right JOIN all_indexes ai
-                         ON ai.index_name = aip.index_name
-                     AND ai.owner = aip.index_owner
-                   WHERE ai.table_name = upper( :p_table )
-                     AND ai.table_owner = upper( :p_owner ))
+                         CASE
+                         WHEN( :p_source_object IS NOT NULL OR :p_partname IS NOT NULL
+                             )
+                     AND ( partitioned = 'YES' )
+                     AND partition_name IS NULL
+                         THEN 'N'
+                         ELSE 'Y'
+                         END include
+                    FROM td_part_gtt JOIN 
+                         (  SELECT DISTINCT CASE :l_part_type WHEN 'subpart' THEN subpartition_name ELSE partition_name END partition_name,
+                                   index_owner,
+                                   index_name,
+                                   CASE :l_part_type WHEN 'subpart' THEN isp.status ELSE ip.status END status
+                              FROM all_ind_partitions ip
+                              left JOIN all_ind_subpartitions isp
+                                   USING (index_owner, index_name, partition_name)
+                         ) aip USING( partition_name )
+                   RIGHT JOIN all_indexes ai
+                         ON ai.index_name = aip.index_name AND ai.owner = aip.index_owner
+                   WHERE ai.table_name = UPPER( :p_table )
+                     AND ai.table_owner = UPPER( :p_owner ))
           WHERE REGEXP_LIKE( index_type, '^' || :p_index_type, 'i' )
             AND REGEXP_LIKE( partitioned,
                              CASE
-                             WHEN REGEXP_LIKE( 'global',
-                                               :p_part_type,
-                                               'i'
-                                             )
+                             WHEN REGEXP_LIKE( 'global', :p_part_type, 'i' )
                              THEN 'NO'
-                             WHEN REGEXP_LIKE( 'local',
-                                               :p_part_type,
-                                               'i'
-                                             )
+                             WHEN REGEXP_LIKE( 'local', :p_part_type, 'i' )
                              THEN 'YES'
                              ELSE '.'
                              END,
                              'i'
                            )
                 -- USE an NVL'd regular expression to determine specific indexes to work on
-            AND REGEXP_LIKE( index_name, nvl( :p_index_regexp, '.' ),
-                             'i' )
+            AND REGEXP_LIKE( index_name, NVL( :p_index_regexp, '.' ), 'i' )
             AND NOT REGEXP_LIKE( index_type, 'iot', 'i' )
-	    AND include = 'Y'
-          ORDER BY idx_ddl_type, partition_position )
+            AND include = 'Y'
+          ORDER BY idx_ddl_type )
  WHERE status IN( 'VALID', 'USABLE', 'N/A' )
