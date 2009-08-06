40f021db1aeee60ed0d19a9438635a5dac7f8f90
Stewart Bryson <stewart.bryson@rittmanmead.com>
2009-08-06 14:30:49 +0000

    New scripts for creating unwrapped archives, as well as the first archive files checked in.

---
+++ cr_evolve_archive.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r ./evolve.zip ./plsql/specs/*.*s ./plsql/bodies/*.*b  ./install/install_evolve.sql ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/bodies/*.*b ./evolve/plsql/functions/*.fnc
