2040ce5a97e687a116398acd24ce404cdb2a44d2
Stewart Bryson <stewart.bryson@rittmanmead.com>
2012-03-12 21:36:33 +0000

    Modified the create_archives.sh script to add the upgrade script to the transcend zip package.
    Recreated the archive files with the new code, plus the new archives.

--- create_archives.sh
+++ create_archives.sh
@@ -8,7 +8,7 @@ zip -r ./evolve_wrapped.zip ./plsql/specs/*.*s ./plsql/wrapped_bodies/*plb  ./in
 
 # create the transcend.zip file
 zip -r ./transcend.zip ./plsql/specs/*.*s ./plsql/bodies/*.*b ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/bodies/*.*b ./evolve/plsql/functions/*.fnc
-zip -r ./transcend.zip ./install/install_transcend.sql ./transcend/plsql/specs/*.*s ./transcend/plsql/bodies/*.*b
+zip -r ./transcend.zip ./install/install_transcend.sql ./install/upgrade_transcend.sql ./transcend/plsql/specs/*.*s ./transcend/plsql/bodies/*.*b
 
 # create the transcend_wrapped.zip file
 zip -r ./transcend_wrapped.zip ./plsql/specs/*.*s ./plsql/wrapped_bodies/*plb ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/wrapped_bodies/*plb
