7cbdc0906d79023079637fcd61e8ce840a1a28ad
Stewart Bryson <stewart.bryson@rittmanmead.com>
2012-03-12 22:18:52 +0000

    Upgrade script wasn't in the wrapped package.

--- create_archives.sh
+++ create_archives.sh
@@ -12,4 +12,4 @@ zip -r ./transcend.zip ./install/install_transcend.sql ./install/upgrade_transce
 
 # create the transcend_wrapped.zip file
 zip -r ./transcend_wrapped.zip ./plsql/specs/*.*s ./plsql/wrapped_bodies/*plb ./evolve/java/* ./evolve/plsql/specs/*.*s ./evolve/plsql/wrapped_bodies/*plb
-zip -r ./transcend_wrapped.zip ./install/install_transcend.sql ./transcend/plsql/specs/*.*s ./transcend/plsql/wrapped_bodies/*plb
+zip -r ./transcend_wrapped.zip ./install/install_transcend.sql ./install/upgrade_transcend.sql ./transcend/plsql/specs/*.*s ./transcend/plsql/wrapped_bodies/*plb
