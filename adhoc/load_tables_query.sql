fb4ee9d7bfee965f62b8ef23593d6ac54a463036
Stewart Bryson <stewart.bryson@rittmanmead.com>
2012-07-04 06:18:19 +0000

    New LOAD_TABLES query.

--- adhoc/load_tables_query.sql
+++ adhoc/load_tables_query.sql
@@ -1,33 +1,39 @@
 SET termout off
 COLUMN disable_ddl format a130
 COLUMN enable_ddl format a130
+COLUMN target_name_old format a35
 
 var p_owner VARCHAR2(30)
 var p_source_owner VARCHAR2(30)
 var p_source_regexp VARCHAR2(100)
 var p_suffix VARCHAR2(30)
 var p_part_tabs VARCHAR2(3)
+var p_source_type VARCHAR2(10)
 
-EXEC :p_source_owner := 'stage';
-EXEC :p_owner := 'stewart';
-EXEC :p_source_regexp := '^tst_.+_src$';
-EXEC :p_suffix := 'src';
+EXEC :p_source_owner := 'testload';
+EXEC :p_owner := 'testload2';
+EXEC :p_source_regexp := '.';
+EXEC :p_suffix := null;
+EXEC :p_source_type := 'view|table';
 
 SET termout on
 
 SELECT *
-  FROM (SELECT owner source_owner,
-	       object_name source_object,
-	       object_type source_object_type,
-	       upper( regexp_replace( object_name, '(.+)(_)(.+)$', '\1'||CASE WHEN :p_suffix IS NULL THEN NULL ELSE '_'||:p_suffix END )) target_name
-	  FROM all_objects 
-	 WHERE object_type IN ( 'TABLE', 'VIEW', 'SYNONYM' )) s
+  FROM ( SELECT owner source_owner,
+	        object_name source_object,
+	        object_type source_object_type,
+                upper( CASE WHEN :p_suffix IS NULL THEN object_name ELSE regexp_replace( object_name, '(_[^_]+)$', NULL ) END ) target_name
+	   FROM all_objects
+          WHERE REGEXP_LIKE( object_type, :p_source_type, 'i' )
+            AND REGEXP_LIKE( object_name, :p_source_regexp, 'i' )
+            AND REGEXP_LIKE( object_name, CASE WHEN :p_suffix IS NULL THEN '.' ELSE '_'||:p_suffix||'$' END, 'i' )
+            AND owner = upper ( :p_source_owner )
+       ) s
   JOIN ( SELECT owner target_owner,
   		object_name target_name,
 		object_type target_oject_type
   	   FROM all_objects
-	  WHERE object_type IN ( 'TABLE' ) ) t 
+	  WHERE object_type IN ( 'TABLE' )
+            AND owner = upper( :p_owner ) 
+       ) t 
        USING (target_name)
- WHERE REGEXP_LIKE( source_object, :p_source_regexp, 'i' )
-   AND source_owner = upper( :p_source_owner )
-   AND target_owner = upper( :p_owner )
